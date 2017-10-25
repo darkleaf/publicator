0b6ff7d3c3f3bd3bec284e3171f19174324ce577
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2017-10-25 21:44:12 +0400

    remove unnecessary ns alias prefix

--- test/publicator/interactors/fixtures.clj
+++ test/publicator/interactors/fixtures.clj
@@ -1,20 +1,19 @@
 (ns publicator.interactors.fixtures
   (:require
-   [publicator.fakes.storage :as fakes.storage]
-   [publicator.fakes.session :as fakes.session]
-   [publicator.fakes.user-queries :as fakes.user-q]
-   [publicator.fakes.hasher :as fakes.hasher]
-   [publicator.fakes.id-generator :as fakes.id-generator]
-
+   [publicator.fakes.storage :as storage]
+   [publicator.fakes.session :as session]
+   [publicator.fakes.user-queries :as user-q]
+   [publicator.fakes.hasher :as hasher]
+   [publicator.fakes.id-generator :as id-generator]
    [clojure.test :as t]))
 
 (defn implementations [f]
-  (let [db (fakes.storage/build-db)
-        binding-map (reduce merge [(fakes.storage/binding-map db)
-                                   (fakes.session/binding-map)
-                                   (fakes.user-q/binging-map db)
-                                   (fakes.hasher/binding-map)
-                                   (fakes.id-generator/binging-map)])]
+  (let [db (storage/build-db)
+        binding-map (reduce merge [(storage/binding-map db)
+                                   (session/binding-map)
+                                   (user-q/binging-map db)
+                                   (hasher/binding-map)
+                                   (id-generator/binging-map)])]
     (with-bindings binding-map
       (f))))
 
