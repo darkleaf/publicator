8a3a2774c07f7bf5b2911198f1e235ac9ad3dc7b
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2017-11-01 18:59:58 +0400

    update clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
                  [better-cond "2.0.1-SNAPSHOT"]
                  [com.stuartsierra/component "0.3.2"]
 
