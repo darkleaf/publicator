ac3d55ff2517ea48f95f382944436a0ab7c05f67
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2017-11-16 23:07:45 +0400

    impl.storage multimethods -> protocol

--- src/publicator/impl/storage.clj
+++ src/publicator/impl/storage.clj
@@ -3,10 +3,37 @@
    [jdbc.core :as jdbc]
    [hugsql.core :as hugsql]
    [medley.core :as medley]
-   [publicator.interactors.abstractions.storage :as storage]
-   [publicator.domain.user :as user])
+   [publicator.interactors.abstractions.storage :as storage])
   (:import
-   [publicator.domain.user User]))
+   [java.util.concurrent TimeoutException]))
+
+(defprotocol Manager
+  (-lock [this conn ids])
+  (-select [this conn ids])
+  (-insert [this conn boxes])
+  (-delete [this conn ids]))
+
+(defn lock [this conn ids]
+  (if (empty? ids)
+    []
+    (-lock this conn ids)))
+
+(defn select [this conn ids]
+  (if (empty? ids)
+    []
+    (-select this conn ids)))
+
+(defn insert [this conn boxes]
+  (if (empty? boxes)
+    nil
+    (-insert this conn boxes)))
+
+(defn delete [this conn ids]
+  (if (empty? ids)
+    nil
+    (-delete this conn ids)))
+
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 (deftype AggregateBox [volatile initial id version]
   clojure.lang.IDeref
@@ -20,7 +47,6 @@
 (defn build-box [state initial id version]
   (AggregateBox. (volatile! state) initial id version))
 
-
 (defn- need-insert? [box]
   (and (some? @box)
        (not= @box (.-initial box))))
@@ -32,31 +58,18 @@
 
 ;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-(defmulti locks-for  (fn [klass conn ids] klass))
-(defmulti select-for (fn [klass conn ids] klass))
-(defmulti insert-for (fn [klass conn boxes] klass))
-(defmulti delete-for (fn [klass conn ids] klass))
-
-;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-(defn- select-all [data-source ids]
-  (with-open [conn (jdbc/connection data-source)]
-    (reduce-kv
-     (fn [acc klass method]
-       (into acc (method klass conn ids)))
-     []
-     (methods select-for))))
-
-(deftype Transaction [data-source boxes]
+(deftype Transaction [data-source managers boxes]
   storage/Transaction
   (-get-many [this ids]
-    (let [for-fetch (remove #(contains? @boxes %) ids)
-          from-db   (select-all data-source ids)
-          indexed   (->> from-db
-                         (group-by storage/id)
-                         (medley/map-vals first))
-          _         (vswap! boxes merge indexed)]
-      (select-keys @boxes ids)))
+    (with-open [conn (jdbc/connection data-source)]
+      (let [for-fetch (remove #(contains? @boxes %) ids)
+            from-db   (->> managers
+                           (vals)
+                           (mapcat #(select % conn ids))
+                           (group-by storage/id)
+                           (medley/map-vals first))
+            _         (vswap! boxes merge from-db)]
+        (select-keys @boxes ids))))
 
   (-create [this state]
     (let [id (:id state)
@@ -64,109 +77,73 @@
       (vswap! boxes assoc id it)
       it)))
 
-(defn- build-tx [data-source]
-  (Transaction. data-source (volatile! {})))
+(defn- build-tx [data-source managers]
+  (Transaction. data-source managers (volatile! {})))
 
-;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-(defn- locks-all [conn ids]
-  (if (empty? ids)
-    {}
-    (do
-      (reduce-kv
-       (fn [acc klass method]
-         (merge acc (method klass conn ids)))
-       {}
-       (methods locks-for)))))
-
-(defn- lock [conn boxes]
+(defn- lock-all [conn managers boxes]
   (let [ids      (->> (vals boxes)
                       (filter need-delete?)
                       (map storage/id))
-        versions (locks-all conn ids)]
+        versions (->> managers
+                      (vals)
+                      (mapcat #(lock % conn ids))
+                      (group-by :id)
+                      (medley/map-vals #(-> % first :version)))]
     (every?
-     #(let [old-version (->> % (get boxes) (storage/version))
-            cur-version (get versions %)]
-        (= old-version cur-version))
+     #(let [initial (->> % (get boxes) (storage/version))
+            current (get versions %)]
+        (= initial current))
      ids)))
 
-(defn- delete [conn boxes]
+(defn- delete-all [conn managers boxes]
   (let [for-delete (filter need-delete? (vals boxes))
         groups     (->> for-delete
                         (group-by #(-> % .-initial class))
-                        (medley/map-vals #(map storage/id %)))]
-    (doseq [[klass ids] groups]
-      (delete-for klass conn ids))))
+                        (medley/map-vals #(map storage/id %))
+                        (medley/map-keys #(get managers %)))]
+    (doseq [[manager ids] groups]
+      (delete manager conn ids))))
 
-(defn- insert [conn boxes]
+(defn- insert-all [conn managers boxes]
   (let [for-insert (filter need-insert? (vals boxes))
         groups     (->> for-insert
-                        (group-by #(-> % deref class)))]
-    (doseq [[klass boxes] groups]
-      (insert-for klass conn boxes))))
+                        (group-by #(-> % deref class))
+                        (medley/map-keys #(get managers %)))]
+    (doseq [[manager boxes] groups]
+      (insert manager conn boxes))))
 
-(defn- commit [tx]
+(defn- commit [tx managers]
   (let [data-source (.-data-source tx)
         boxes     @(.-boxes tx)]
     (with-open [conn (jdbc/connection data-source)]
       (jdbc/atomic conn
-                   (when (lock conn boxes)
-                     (delete conn boxes)
-                     (insert conn boxes)
+                   (when (lock-all conn managers boxes)
+                     (delete-all conn managers boxes)
+                     (insert-all conn managers boxes)
                      true)))))
 
 (defn- timestamp []
   (inst-ms (java.util.Date.)))
 
-(defn- wrap-tx [data-source body stop-after]
-  (let [tx       (build-tx data-source)
+(defn- wrap-tx [data-source managers body stop-after]
+  (let [tx       (build-tx data-source managers)
         res      (body tx)
-        success? (commit tx)]
+        success? (commit tx managers)]
     (cond
       success? res
-      (< (timestamp) stop-after) (recur data-source body stop-after)
-      ;;TODO: may be chose another excepion class?
-      :else (throw (java.util.concurrent.TimeoutException. "Can't retry transaction")))))
+      (< (timestamp) stop-after) (recur data-source managers body stop-after)
+      :else (throw (TimeoutException. "Can't retry transaction")))))
 
-(deftype Storage [data-source opts]
+(deftype Storage [data-source managers opts]
   storage/Storage
   (-wrap-tx [this body]
     (let [soft-timeout (get opts :soft-timeout 500)
           stop-after   (+ (timestamp) soft-timeout)]
-      (wrap-tx data-source body stop-after))))
+      (wrap-tx data-source managers body stop-after))))
 
-(defn binding-map
-  ([data-source]
-   (binding-map data-source {}))
-  ([data-source opts]
-   {#'storage/*storage* (Storage. data-source opts)}))
-
-;;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-(hugsql/def-db-fns "db/storage/user.sql" {:quoting :ansi})
-
-(defmethod insert-for User [_ conn boxes]
-  (user-insert conn {:vals (map #(-> % deref vals) boxes)}))
-
-(defn- get-version [row]
-  (-> row :version .getValue))
-
-(defn- row->user-box [row]
-  (let [version (get-version row)
-        row     (dissoc row :version)
-        user    (user/map->User row)]
-     (build-box user user (:id user) version)))
-
-(defmethod select-for User [_ conn ids]
-  (map row->user-box (user-select conn {:ids ids})))
-
-(defmethod delete-for User [_ conn ids]
-  (user-delete conn {:ids ids}))
-
-(defn- lock-row->pair [row]
-  (let [id      (:id row)
-        version (get-version row)]
-    [id version]))
-
-(defmethod locks-for User [_ conn ids]
-  (into {} (map lock-row->pair (user-locks conn {:ids ids}))))
+(defn binding-map [data-source managers opts]
+  {:pre [(map? managers)
+         (every? class? (keys managers))
+         (every? #(satisfies? Manager %) (vals managers))
+         (map? opts)]}
+  {#'storage/*storage* (Storage. data-source managers opts)})
--- test/publicator/impl/storage_test.clj
+++ test/publicator/impl/storage_test.clj
@@ -3,15 +3,54 @@
    [clojure.test :as t]
    [hugsql.core :as hugsql]
    [jdbc.core :as jdbc]
-   [publicator.impl.storage :as sut]
+   [publicator.impl.storage :as impl.storage]
    [publicator.impl.test-db :as test-db]
    [publicator.domain.abstractions.aggregate :as aggregate]
    [publicator.interactors.abstractions.storage :as storage]))
 
+(defrecord TestEntity [id counter]
+  aggregate/Aggregate
+  (spec [_] any?))
+
+(defn build-test-entity []
+  (TestEntity. 1 0))
+
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 (hugsql/def-db-fns "publicator/impl/storage_test.sql")
 
+(defn- get-version [row]
+  (-> row :version .getValue))
+
+(defn- row->box [row]
+  (let [version (get-version row)
+        row     (dissoc row :version)
+        entity  (map->TestEntity row)]
+     (impl.storage/build-box entity entity (:id entity) version)))
+
+(defn- lock-row->map [row]
+  (let [id      (:id row)
+        version (get-version row)]
+    {:id id, :version version}))
+
+(deftype TestEntityManager []
+  impl.storage/Manager
+  (-lock [_ conn ids]
+    (map lock-row->map (test-entity-locks conn {:ids ids})))
+  (-select [_ conn ids]
+    (map row->box (test-entity-select conn {:ids ids})))
+  (-insert [_ conn boxes]
+    (test-entity-insert conn {:vals (map #(-> % deref vals) boxes)}))
+  (-delete [_ conn ids]
+    (test-entity-delete conn {:ids ids})))
+
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 (defn- setup [t]
-  (with-bindings (sut/binding-map test-db/data-source)
+  (with-bindings (impl.storage/binding-map
+                  test-db/data-source
+                  {TestEntity (TestEntityManager.)}
+                  {})
     (t)))
 
 (defn- table [t]
@@ -26,45 +65,6 @@
   test-db/clear-fixture
   setup
   table)
-
-;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-(defrecord TestEntity [id counter]
-  aggregate/Aggregate
-  (spec [_] any?))
-
-(defn build-test-entity []
-  (TestEntity. 1 0))
-
-;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-(defmethod sut/insert-for TestEntity [_ conn boxes]
-  (test-entity-insert conn {:vals (map #(-> % deref vals) boxes)}))
-
-(defn- get-version [row]
-  (-> row :version .getValue))
-
-(defn- row->box [row]
-  (let [version (get-version row)
-        row     (dissoc row :version)
-        entity  (map->TestEntity row)]
-     (sut/build-box entity entity (:id entity) version)))
-
-(defmethod sut/select-for TestEntity [_ conn ids]
-  (map row->box (test-entity-select conn {:ids ids})))
-
-(defmethod sut/delete-for TestEntity [_ conn ids]
-  (test-entity-delete conn {:ids ids}))
-
-(defn- lock-row->pair [row]
-  (let [id      (:id row)
-        version (get-version row)]
-    [id version]))
-
-(defmethod sut/locks-for TestEntity [_ conn ids]
-  (into {} (map lock-row->pair (test-entity-locks conn {:ids ids}))))
-
-;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 (t/deftest create
   (let [entity (storage/tx-create (build-test-entity))]
@@ -100,29 +100,3 @@
                     (doall))
         entity (storage/tx-get-one id)]
     (t/is (= n (:counter entity)))))
-
-
-;; (t/deftest user
-;;   (t/testing "create"
-;;     (let [user (factories/create-user)]
-;;       (t/is (some? user))
-;;       (t/is (some? (storage/tx-get-one (:id user))))))
-;;   (t/testing "update"
-;;     (let [user      (factories/create-user)
-;;           full-name "new full-name"
-;;           _         (storage/tx-swap! (:id user) assoc :full-name full-name)
-;;           user      (storage/tx-get-one (:id user))]
-;;       (t/is (= full-name (:full-name user)))))
-;;   (t/testing "destroy"
-;;     (let [user (factories/create-user)
-;;           _    (storage/tx-destroy!  (:id user))
-;;           user (storage/tx-get-one (:id user))]
-;;       (t/is (nil? user)))))
-
-;; (t/deftest special
-;;   (t/testing "create and destroy"
-;;     (let [user-id (storage/with-tx t
-;;                     (let [user (storage/create t (factories/build-user))]
-;;                       (storage/destroy! user)
-;;                       (storage/id user)))]
-;;       (t/is (nil? (storage/tx-get-one user-id))))))
