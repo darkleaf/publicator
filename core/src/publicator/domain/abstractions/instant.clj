56908977da67fcb6743030123e0d3e13b3d55c0d
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-05-19 22:44:15 +0400

    fix instant

--- core/src/publicator/domain/abstractions/instant.clj
+++ core/src/publicator/domain/abstractions/instant.clj
@@ -1,6 +1,14 @@
 (ns publicator.domain.abstractions.instant
+  (:require
+   [clojure.spec.alpha :as s])
   (:import
    [java.time Instant]))
 
-(defn ^:dynamic now []
+(defn ^:dynamic *now* []
   (Instant/now))
+
+(s/fdef now
+  :ret inst?)
+
+(defn now []
+  (*now*))
