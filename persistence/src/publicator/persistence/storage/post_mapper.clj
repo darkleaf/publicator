d857d826f3a8ed7319b4e3e762bb2f408a348038
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-06-05 23:47:43 +0400

    refactoring

--- persistence/src/publicator/persistence/storage/post_mapper.clj
+++ persistence/src/publicator/persistence/storage/post_mapper.clj
@@ -11,35 +11,35 @@
 (hugsql/def-db-fns "publicator/persistence/storage/post_mapper.sql"
   {:adapter (cj-adapter/hugsql-adapter-clojure-jdbc)})
 
-(defn- get-version [row]
-  (-> row :version .getValue))
+(defn- sql->version [raw]
+  (.getValue raw))
 
-(defn- row->state [row]
-  (let [version (get-version row)
-        row     (-> row
-                    (dissoc :version)
-                    (update :created-at #(.toInstant %)))
-        state   (post/map->Post row)]
-    {:state state, :version version}))
+(defn- sql->aggretate [raw]
+  (-> raw
+      (update :created-at #(.toInstant %))
+      (post/map->Post)))
 
-(defn- lock-row->map [row]
-  (let [id      (:id row)
-        version (get-version row)]
-    {:id id, :version version}))
-
-(defn- state->values [state]
-  (-> state
+(defn- aggregate->sql [aggregate]
+  (-> aggregate
       (update :created-at #(Timestamp/from %))
-      vals))
+      (vals)))
+
+(defn- row->versioned-aggregate [row]
+  {:aggregate (-> row (dissoc :version) sql->aggretate)
+   :version   (-> row (get :version) sql->version)})
+
+(defn- row->versioned-id [{:keys [id version]}]
+  {:id      id
+   :version (sql->version version)})
 
 (deftype PostMapper []
   persistence.storage/Mapper
   (-lock [_ conn ids]
-    (map lock-row->map (post-locks conn {:ids ids})))
+    (map row->versioned-id (post-locks conn {:ids ids})))
   (-select [_ conn ids]
-    (map row->state (post-select conn {:ids ids})))
-  (-insert [_ conn states]
-    (post-insert conn {:vals (map state->values states)}))
+    (map row->versioned-aggregate (post-select conn {:ids ids})))
+  (-insert [_ conn aggregates]
+    (post-insert conn {:vals (map aggregate->sql aggregates)}))
   (-delete [_ conn ids]
     (post-delete conn {:ids ids})))
 
