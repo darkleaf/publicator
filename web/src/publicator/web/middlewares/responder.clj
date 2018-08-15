52561410ccf1fa2e3608ebd4aac3a7bff286f5c0
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-08-15 15:48:23 +0400

    Revert "all responders"
    This reverts commit dc16400e6d012ce953f6e861769d9f1cb4c9c6da.

--- web/src/publicator/web/middlewares/responder.clj
+++ web/src/publicator/web/middlewares/responder.clj
@@ -1,7 +1,15 @@
 (ns publicator.web.middlewares.responder
   (:require
    [publicator.web.responders.base :as responders.base]
-   [publicator.web.responders.all]))
+
+   [publicator.web.responders.pages.root]
+   [publicator.web.responders.user.log-in]
+   [publicator.web.responders.user.log-out]
+   [publicator.web.responders.user.register]
+   [publicator.web.responders.post.list]
+   [publicator.web.responders.post.show]
+   [publicator.web.responders.post.create]
+   [publicator.web.responders.post.update]))
 
 (defn wrap-reponder [handler]
   (fn [req]
