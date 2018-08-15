70ad999a1b93fee414d9926ebe0a3789024b1e65
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-08-15 15:51:42 +0400

    Revert "all routes"
    This reverts commit d9215f1d32790853bc0f59b135620653a7fd62b0.

--- web/src/publicator/web/routing.clj
+++ web/src/publicator/web/routing.clj
@@ -2,9 +2,27 @@
   (:require
    [sibiro.core]
    [sibiro.extras]
-   [publicator.web.controllers.routes :as routes]))
+   [clojure.set :as set]
+   [publicator.web.controllers.pages.root :as pages.root]
+   [publicator.web.controllers.user.log-in :as user.log-in]
+   [publicator.web.controllers.user.log-out :as user.log-out]
+   [publicator.web.controllers.user.register :as user.register]
+   [publicator.web.controllers.post.list :as post.list]
+   [publicator.web.controllers.post.show :as post.show]
+   [publicator.web.controllers.post.create :as post.create]
+   [publicator.web.controllers.post.update :as post.update]))
 
-(def routes (sibiro.core/compile-routes routes/routes))
+(def routes
+  (sibiro.core/compile-routes
+   (set/union
+    pages.root/routes
+    user.log-in/routes
+    user.log-out/routes
+    user.register/routes
+    post.list/routes
+    post.show/routes
+    post.create/routes
+    post.update/routes)))
 
 (def handler (sibiro.extras/make-handler routes))
 
