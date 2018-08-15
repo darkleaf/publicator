96294c22331801f29300ecb00a8231129d47c388
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-08-15 10:45:25 +0400

    root controller

--- web/src/publicator/web/controllers/user/log_out.clj
+++ web/src/publicator/web/controllers/user/log_out.clj
@@ -1,8 +1,6 @@
 (ns publicator.web.controllers.user.log-out
   (:require
-   [publicator.use-cases.interactors.user.log-out :as interactor]
-   [publicator.web.controllers.base :as base]
-   [publicator.web.url-helpers :as url-helpers]))
+   [publicator.use-cases.interactors.user.log-out :as interactor]))
 
 (defn process [_]
   [interactor/process])
