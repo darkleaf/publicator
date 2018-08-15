71b91e1bc7d53894b37e55c4af13a5990197ceaf
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-08-15 13:06:26 +0400

    handler

--- web/src/publicator/web/handler.clj
+++ web/src/publicator/web/handler.clj
@@ -1,14 +1,25 @@
 (ns publicator.web.handler
   (:require
-   [sibiro.extras]
    [ring.middleware.params :as ring.params]
    [ring.middleware.keyword-params :as ring.keyword-params]
    [ring.middleware.anti-forgery :as ring.anti-forgery]
    [ring.util.request :as ring.request]
    [publicator.web.routing :as routing]
+   [publicator.web.responders.base :as responders.base]
    [publicator.web.middlewares.layout :as layout]
    [publicator.web.middlewares.session :as session]
-   [publicator.web.transit :as t]))
+   [publicator.web.transit :as t]
+
+
+   [publicator.web.responders.pages.root]
+   [publicator.web.responders.user.log-in]
+   [publicator.web.responders.user.log-out]
+   [publicator.web.responders.user.register]
+   [publicator.web.responders.post.list]
+   [publicator.web.responders.post.show]
+   [publicator.web.responders.post.create]
+   [publicator.web.responders.post.update]))
+
 
 (defn- wrap-transit-params [handler]
   (fn [req]
@@ -20,15 +31,21 @@
                 req)]
       (handler req))))
 
+(defn- wrap-reponders [handler]
+  (fn [req]
+    (let [[interactor & args] (handler req)
+          result              (apply interactor args)]
+      (responders.base/->resp result args))))
+
 (defn build
   ([] (build {}))
   ([config]
-   (let [handler (sibiro.extras/make-handler routing/routes)
-         test?   (:test? config)]
-     (cond-> handler
-       true        layout/wrap
-       (not test?) ring.anti-forgery/wrap-anti-forgery
-       true        (session/wrap (:session config {}))
-       true        wrap-transit-params
-       true        ring.keyword-params/wrap-keyword-params
-       true        ring.params/wrap-params))))
+   (let [handler (sibiro.extras/make-handler routing/routes)]
+     (-> routing/handler
+         wrap-reponders
+         layout/wrap
+         ring.anti-forgery/wrap-anti-forgery
+         (session/wrap (:session config {}))
+         wrap-transit-params
+         ring.keyword-params/wrap-keyword-params
+         ring.params/wrap-params))))
