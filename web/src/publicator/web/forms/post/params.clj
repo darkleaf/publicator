8a169763f8b2ee45a74f6d111f022daed6ea7db8
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-08-17 21:26:25 +0400

    fix post form

--- web/src/publicator/web/forms/post/params.clj
+++ web/src/publicator/web/forms/post/params.clj
@@ -1,13 +1,24 @@
-(ns publicator.web.forms.post.params)
+(ns publicator.web.forms.post.params
+  (:require
+   [publicator.web.routing :as routing]))
 
 (defn description [{:keys [url method]}]
   {:widget :submit, :name "Готово"
-   :url url, :method method :nested
+   :url url, :method method, :nested
    {:widget :group, :nested
     [:title {:widget :input, :label "Заголовок"}
      :content {:widget :textarea, :label "Содержание"}]}})
 
-(defn build [cfg initial-params]
-  {:initial-data initial-params
-   :errors       {}
-   :description  (description cfg)})
+(defn build-create [initial-params]
+  (let [cfg {:url    (routing/path-for :post.create/process)
+             :method :post}]
+    {:initial-data initial-params
+     :errors       {}
+     :description  (description cfg)}))
+
+(defn build-update [id initial-params]
+  (let [cfg {:url    (routing/path-for :post.update/process {:id (str id)})
+             :method :post}]
+    {:initial-data initial-params
+     :errors       {}
+     :description  (description cfg)}))
--- web/src/publicator/web/responders/post/create.clj
+++ web/src/publicator/web/responders/post/create.clj
@@ -3,13 +3,10 @@
    [publicator.use-cases.interactors.post.create :as interactor]
    [publicator.web.responders.base :as responders.base]
    [publicator.web.responses :as responses]
-   [publicator.web.forms.post.params :as form]
-   [publicator.web.routing :as routing]))
+   [publicator.web.forms.post.params :as form]))
 
 (defmethod responders.base/result->resp ::interactor/initial-params [[_ params]]
-  (let [cfg  {:url    (routing/path-for :post.create/process)
-              :method :post}
-        form (form/build cfg params)]
+  (let [form (form/build-create params)]
     (responses/render-form form)))
 
 (derive ::interactor/processed ::responders.base/redirect-to-root)
