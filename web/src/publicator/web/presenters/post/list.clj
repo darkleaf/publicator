e23c3ce09795226fdfb82231419bea63c1cae09b
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-08-20 23:08:47 +0400

    presenters

--- web/src/publicator/web/presenters/post/list.clj
+++ web/src/publicator/web/presenters/post/list.clj
@@ -1,22 +1,25 @@
 (ns publicator.web.presenters.post.list
   (:require
    [publicator.use-cases.interactors.post.list :as interactor]
-   [publicator.use-cases.interactors.post.create :as create-interactor]
+   [publicator.use-cases.interactors.post.create :as interactors.post.create]
+   [publicator.use-cases.interactors.post.update :as interactors.post.update]
    [publicator.domain.aggregates.user :as user]
    [publicator.web.routing :as routing]))
 
-(defn- post->model [post]
+(defn- post->model [post authorization]
   {:id             (:id post)
    :url            (routing/path-for :post.show/process {:id (-> post :id str)})
-   :edit-url       (routing/path-for :post.update/initial-params  {:id (-> post :id str)})
+   :update-url     (routing/path-for :post.update/initial-params {:id (-> post :id str)})
    :title          (:title post)
-   :can-edit?      (::interactor/can-edit? post)
+   :can-update?    (= [::interactors.post.update/authorized] authorization)
    :user-full-name (::user/full-name post)})
 
 (defn processed [posts]
-  (let [can-create? (= [::create-interactor/authorized]
-                       (create-interactor/authorize))]
+  (let [authorizations (interactors.post.update/authorize (map :id posts))
+        view-models    (map post->model posts authorizations)
+        can-create?    (= [::interactors.post.create/authorized]
+                          (interactors.post.create/authorize))]
     (cond-> {}
-      :always     (assoc :posts (map post->model posts))
+      :always     (assoc :posts view-models)
       can-create? (assoc :new {:text "New"
                                :url  (routing/path-for :post.create/initial-params)}))))
