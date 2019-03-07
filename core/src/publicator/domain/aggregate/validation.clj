4ebe760880077575d4c656089bfcfc45fb44b724
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-03-07 21:56:07 +0400

    db -> agg in validation

--- core/src/publicator/domain/aggregate/validation.clj
+++ core/src/publicator/domain/aggregate/validation.clj
@@ -13,60 +13,60 @@
   (fnil d/q (d/empty-db)))
 
 
-(defn- without-attribute-errors [db ids attribute]
+(defn- without-attribute-errors [agg ids attribute]
   (q '{:find  [[?e ...]]
        :in    [$ [?e ...] ?a]
        :where [(not-join [?e ?a]
                  [?err :entity ?e]
                  [?err :attribute ?a])]}
-     (-> db meta :aggregate/errors) ids attribute))
+     (-> agg meta :aggregate/errors) ids attribute))
 
-(defn- check-attribute [db ids [attr pred & args]]
-  (let [args   (vec args)
-        ids    (without-attribute-errors db ids attr)
-        errors (q '{:find  [?e ?a ?v ?pred ?args]
-                    :in    [$ [?e ...] ?a ?pred ?args]
-                    :where [[?e ?a ?v]
-                            (not [(clojure.core/apply ?pred ?v ?args)])]}
-                  db ids attr pred args)
+(defn- check-attribute [agg ids [attr pred & args]]
+  (let [args    (vec args)
+        ids     (without-attribute-errors agg ids attr)
+        errors  (q '{:find  [?e ?a ?v ?pred ?args]
+                     :in    [$ [?e ...] ?a ?pred ?args]
+                     :where [[?e ?a ?v]
+                             (not [(clojure.core/apply ?pred ?v ?args)])]}
+                   agg ids attr pred args)
         tx-data (for [error errors]
                   (-> (zipmap [:entity :attribute :value :predicate :args]
                               error)
                       (assoc :type ::predicate)))]
-    (vary-meta db update :aggregate/errors db-with tx-data)))
+    (vary-meta agg update :aggregate/errors db-with tx-data)))
 
-(defn- check-required [db ids [attr & _]]
-  (let [ids    (without-attribute-errors db ids attr)
-        errors (d/q '{:find  [?e ?a]
-                      :in    [$ [?e ...] ?a]
-                      :where [[(missing? $ ?e ?a)]]}
-                    db ids attr)
+(defn- check-required [agg ids [attr & _]]
+  (let [ids     (without-attribute-errors agg ids attr)
+        errors  (d/q '{:find  [?e ?a]
+                       :in    [$ [?e ...] ?a]
+                       :where [[(missing? $ ?e ?a)]]}
+                     agg ids attr)
         tx-data (for [error errors]
                   (-> (zipmap [:entity :attribute]
                               error)
                       (assoc :type ::required)))]
-    (vary-meta db update :aggregate/errors db-with tx-data)))
+    (vary-meta agg update :aggregate/errors db-with tx-data)))
 
-(defn types [db & checks]
+(defn types [agg & checks]
   (let [ids (d/q '{:find  [[?e ...]]
                    :where [[?e _ _]]}
-                 db)]
-    (reduce (fn [db check] (check-attribute db ids check))
-            db checks)))
+                 agg)]
+    (reduce (fn [agg check] (check-attribute agg ids check))
+            agg checks)))
 
-(defn required-for [db entities-q & checks]
-  (let [ids (d/q entities-q db)]
-    (reduce (fn [db check]
-              (-> db
+(defn required-for [agg entities-q & checks]
+  (let [ids (d/q entities-q agg)]
+    (reduce (fn [agg check]
+              (-> agg
                   (check-required ids check)
                   (check-attribute ids check)))
-            db checks)))
+            agg checks)))
 
-(defn query [db entities-q query pred & args]
+(defn query [agg entities-q query pred & args]
   (let [args        (vec args)
-        ids         (d/q entities-q db)
+        ids         (d/q entities-q agg)
         value-by-id (for [id ids]
-                      [id (d/q query db id)])
+                      [id (d/q query agg id)])
         with-errs   (remove (fn [[id value]] (apply pred value args))
                             value-by-id)
         tx-data     (for [[id value] with-errs]
@@ -76,7 +76,7 @@
                        :predicate pred
                        :args      args
                        :type      ::query})]
-    (vary-meta db update :aggregate/errors db-with tx-data)))
+    (vary-meta agg update :aggregate/errors db-with tx-data)))
 
 
 
