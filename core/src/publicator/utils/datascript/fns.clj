d302aaed5433f7879b6870d4a971467946671af2
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-03-09 22:22:51 +0400

    assoc-in utility fn

--- core/src/publicator/utils/datascript/fns.clj
+++ core/src/publicator/utils/datascript/fns.clj
@@ -12,3 +12,8 @@
         add     (for [[e v] e-v]
                   [:db/add e attribute (apply f v args)])]
     (concat retract add)))
+
+(defn assoc-in [db entities-q attr value]
+  (let [ids (d/q entities-q db)]
+    (for [id ids]
+      [:db/add id attr value])))
--- core/test/publicator/utils/datascript/fns_test.clj
+++ core/test/publicator/utils/datascript/fns_test.clj
@@ -18,3 +18,21 @@
                                  [:db/add 2 :counters 11]
                                  [:db/add 2 :counters 21]]))]
     (t/is (= expected actual))))
+
+(t/deftest assoc-in
+  (let [inner-q  '{:find  [[?e ...]]
+                   :where [[?e :inner/base :root]]}
+        schema   {:inner/base {:db/valueType :db.type/ref}}
+        actual   (-> (d/empty-db schema)
+                     (d/db-with [[:db/add 1 :db/ident :root]
+                                 [:db/add 2 :inner/base 1]
+                                 [:db/add 3 :inner/base 1]])
+                     (d/db-with [[:db.fn/call d.fns/assoc-in inner-q :attr :val]]))
+        expected (-> (d/empty-db schema)
+                     (d/db-with [[:db/add 1 :db/ident :root]
+                                 [:db/add 2 :inner/base 1]
+                                 [:db/add 3 :inner/base 1]
+
+                                 [:db/add 2 :attr :val]
+                                 [:db/add 3 :attr :val]]))]
+    (t/is (= expected actual))))
