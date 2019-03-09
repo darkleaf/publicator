4c563ea707db4efa4a7f3ce599f72bab5fc74c4a
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-03-09 19:07:48 +0400

    new aggregate

--- core/src/publicator/domain/aggregate.clj
+++ core/src/publicator/domain/aggregate.clj
@@ -1,64 +1,133 @@
 (ns publicator.domain.aggregate
   (:require
    [publicator.domain.abstractions.instant :as instant]
-   [publicator.utils.datascript.validation :as validation]
+   [publicator.utils.datascript.validation :as d.validation]
    [datascript.core :as d]))
-
-(defmulti schema identity)
-(defmethod schema :default [_] {})
-
-(def ^:const root-q '{:find [[?e ...]]
-                      :where [[?e :db/ident :root]]})
-
-(def ^:private validator
-  (validation/compose
-   (validation/attributes [:root/id         pos-int?]
-                          [:root/created-at inst?]
-                          [:root/updated-at inst?])
-   (validation/in-case-of root-q
-                          [:root/id         some?]
-                          [:root/created-at some?]
-                          [:root/updated-at some?])))
-
-(defn- check-errors! [aggregate]
-  (if-let [errs (-> aggregate meta :aggregate/errors seq)]
-    (throw (ex-info "Aggregate has errors" {:type   ::has-errors
-                                            :errors errs}))))
 
 (defn root [aggregate]
   (d/entity aggregate :root))
 
-(defn allocate [type id]
-  (let [s (merge (schema type)
-                 {:root/id {:db/unique :db.unique/identity}})]
-    (-> (d/empty-db s)
-        (d/db-with [{:db/ident :root
-                     :root/id  id}])
-        (vary-meta assoc
-                   :type type
-                   :aggregate/errors (d/empty-db)))))
+(def ^:const root-q '{:find [[?e ...]]
+                      :where [[?e :db/ident :root]]})
 
-(defn build [type id tx-data]
-  (let [tx-data   (concat tx-data
-                          [[:db/add :root :root/created-at (instant/*now*)]
-                           [:db/add :root :root/updated-at (instant/*now*)]
+(def base-spec
+  {:schema        {:root/id {:db/unique :db.unique/identity}}
+   :on-build-tx   (fn [] [[:db/add 1 :db/ident :root]
+                          [:db/add :root :root/created-at (instant/*now*)]])
+   :additional-tx (fn [] [[:db/add :root :root/updated-at (instant/*now*)]])
+   :read-only     #{:root/id :root/created-at}
+   :validator     (d.validation/compose
+                   (d.validation/attributes [:root/id         pos-int?]
+                                            [:root/created-at inst?]
+                                            [:root/updated-at inst?])
+                   (d.validation/in-case-of root-q
+                                            [:root/id         some?]
+                                            [:root/created-at some?]
+                                            [:root/updated-at some?]))})
 
-                           [:db.fn/call check-errors!]])
-        aggregate (allocate type id)
-        report    (d/with aggregate tx-data)
-        tx-data   (:tx-data report)
-        aggregate (:db-after report)]
-    (vary-meta aggregate assoc :aggregate/tx-data tx-data)))
+;; (defn- check-errors! [aggregate]
+;;   (if-let [errs (-> aggregate meta :aggregate/errors seq)]
+;;     (throw (ex-info "Aggregate has errors" {:type   ::has-errors
+;;                                             :errors errs}))))
 
-(defn change [aggregate tx-data]
-  (let [tx-data   (concat tx-data
-                          [[:db/add :root :root/updated-at (instant/*now*)]
-                           [:db.fn/call check-errors!]])
-        report    (d/with aggregate tx-data)
-        tx-data   (:tx-data report)
-        aggregate (:db-after report)]
-    (vary-meta aggregate assoc :aggregate/tx-data tx-data)))
+(defn extend-spec [spec other]
+  (cond-> spec
+    (contains? other :type)          (assoc :type (:type other))
+    :always                          (update :schema
+                                             merge (:schema other))
+    (contains? other :on-build-tx)   (update :on-build-tx
+                                             (fn [old] #(concat (old) ((:on-build-tx other)))))
+    (contains? other :additional-tx) (update :additional-tx
+                                             (fn [old] #(concat (old) ((:additional-tx other)))))
+    :always                          (update :read-only
+                                             into (:read-only other))
+    (contains? other :validator)     (update :validator
+                                             validation/compose (:validator other))))
+
+
+;; (defn becomes [agg new-spec])  ????
+
+;; (defn allocate [spec tx-data]
+;;   (let [spec (extend-spec base-spec spec)
+;;         agg (-> (d/empty-db (:schema spec))
+;;                 (d/db-with tx-data))]))
+
+
+    ;; (-> (d/empty-db s)
+    ;;     (d/db-with [{:db/ident :root
+    ;;                  :root/id  id}])
+    ;;     (vary-meta assoc
+    ;;                :type type))))
+
+(defn build [spec tx-data]
+  (let [spec    (extend-spec base-spec spec)
+        tx-data (concat ((:on-build-tx spec))
+                        tx-data
+                        ((:additional-tx spec)))
+        agg     (d/empty-db (:schema spec))
+        report  (d/with agg tx-data)
+        agg     (:db-after report)
+        changes (:tx-data report)
+        errors  (d.validation/validate agg (:validator spec))]
+    (vary-meta agg merge {:type              (:type spec)
+                          :aggregate/changes changes
+                          :aggregate/errors  errors})))
+
+
+
+  ;;       tx-data   (:tx-data report)
+  ;;       aggregate (:db-after report)
+  ;;       errors    (validation/validate aggregate
+  ;;                                      (validation/compose
+  ;;                                       base-validator
+  ;;                                       (validator (type aggregate))))]
+  ;;   (vary-meta aggregate assoc :aggregate/tx-data tx-data)))
+
+;; (defn change [aggregate tx-data]
+;;   (let [tx-data   (concat tx-data
+;;                           [[:db/add :root :root/updated-at (instant/*now*)]
+;;                            [:db.fn/call check-errors!]])
+;;         report    (d/with aggregate tx-data)
+;;         tx-data   (:tx-data report)
+;;         aggregate (:db-after report)]
+;;     (vary-meta aggregate assoc :aggregate/tx-data tx-data)))
+
+
+
+
+
+
+
+
+
 
 
 ;; (defn build! [])
 ;; (defn change! [])
+
+
+
+
+
+;; (def spec
+;;   {:type          :user
+;;    ;; :space     :user, надо выпиливать эти спэйсы и переходить на полиморфные связи, больно сложно
+;;    :schema        {}
+;;    :on-create-tx  (fn [] [[:db/add :root :root/id 1]])
+;;    :additional-tx (fn [] [[:db.fn/call]])
+;;    :validator     nil
+;;    :read-only     #{:root/id}})
+
+;; (allocate spec)
+;; (create spec tx-data)
+;; (create! spec tx-data)
+;; (change agg tx-data)
+;; (change! agg tx-data)
+
+
+;; ;; при этом можно замержить валидатор или коллбэк для формы
+;; ;; можно даже наследовать
+;; (storage/*get* user/spec 1)
+
+
+;; spec нужно в metadata цеплять
--- core/test/publicator/domain/aggregate_test.clj
+++ core/test/publicator/domain/aggregate_test.clj
@@ -1,34 +1,66 @@
 (ns publicator.domain.aggregate-test
   (:require
-   [publicator.domain.aggregate :as sut]
+   [publicator.domain.aggregate :as agg]
    [publicator.domain.abstractions.scaffolding :as scaffolding]
+   [publicator.utils.datascript.fns :as d.fns]
+   [publicator.utils.datascript.validation :as d.validation]
    [clojure.test :as t]))
 
 (t/use-fixtures :each scaffolding/setup)
 
-(defmethod sut/schema ::aggregate [_]
-  {:inner/base {:db/valueType :db.type/ref}})
-
 (def ^:const id 42)
 
-(t/deftest build
-  (let [aggregate (sut/build ::aggregate id
-                             [{:db/ident :root
-                               :root/key :val}
-                              {:inner/base :root
-                               :inner/key  :inner-val}])]
-    (t/testing "id"
-      (t/is (= id (-> aggregate sut/root :root/id))))
-    (t/testing "type"
-      (t/is (= ::aggregate (type aggregate))))
-    (t/testing "root"
-      (t/is (= :inner-val (-> aggregate sut/root :inner/_base first :inner/key))))))
+(def spec
+  {:type          :test-agg
+   :schema        {:inner/base {:db/valueType :db.type/ref}}
+   :on-build-tx   (fn [] [[:db/add :root :root/id id]
+                          [:db/add :root :test-agg/version 0]
+                          [:db/add :root :test-agg/read-only 0]])
+   :additional-tx (fn [] [[:db.fn/call d.fns/update-all :test-agg/version inc]])
+   :validator     (d.validation/attributes [:test-agg/key keyword?]
+                                           [:test-agg/version pos-int?]
+                                           [:teat-agg/read-only pos-int?]
+                                           [:inner/key keyword?])
+   :read-only     #{:test-agg/read-only}})
 
-(t/deftest change
-  (let [aggregate (sut/build ::aggregate id
-                             [{:db/ident :root
-                               :root/key :val}
-                              {:inner/base :root
-                               :inner/key  :inner-val}])
-        aggregate (sut/change aggregate [[:db/add :root :root/key :new-val]])]
-    (t/is (= :new-val (-> aggregate sut/root :root/key)))))
+;; (t/deftest allocate
+;;   (let [tx-data [[:db/add :root :root/id 1]]
+;;         agg     (aggregate/allocate spec tx-data)]
+;;     (t/is (some? agg))))
+
+(t/deftest build
+  (t/testing "main path"
+    (let [agg (agg/build spec
+                         [[:db/add :root :test-agg/key :val]
+                          [:db/add "inner" :inner/base :root]
+                          [:db/add "inner" :inner/key :inner-val]])]
+      (t/testing "some"
+        (t/is (some? agg)))
+      (t/testing "id"
+        (t/is (= id (-> agg agg/root :root/id))))
+      (t/testing "type"
+        (t/is (= :test-agg (type agg))))
+      (t/testing :schema
+        (t/is (= :inner-val (-> agg agg/root :inner/_base first :inner/key))))
+      (t/testing :additional-tx
+        (t/is (= 1 (-> agg agg/root :test-agg/version))))
+      (t/testing :aggregate/changes
+        (t/is (not-empty (-> agg meta :aggregate/changes))))
+      (t/testing :aggregate/errors
+        (t/is (empty? (-> agg meta :aggregate/errors))))))
+  (t/testing "errors"
+    (let [agg (agg/build spec
+                         [[:db/add :root :test-agg/key "wrong"]])]
+      (t/is (not-empty (-> agg meta :aggregate/errors))))))
+
+
+;; (t/deftest build!)
+
+;; (t/deftest change
+;;   (let [aggregate (sut/build ::aggregate id
+;;                              [{:db/ident :root
+;;                                :root/key :val}
+;;                               {:inner/base :root
+;;                                :inner/key  :inner-val}])
+;;         aggregate (sut/change aggregate [[:db/add :root :root/key :new-val]])]
+;;     (t/is (= :new-val (-> aggregate sut/root :root/key)))))
