2c08ba74b1858d321bdb73a68fe01cf23ca09802
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-03-27 22:58:44 +0400

    aggregate

--- core/src/publicator/domain/aggregate.clj
+++ core/src/publicator/domain/aggregate.clj
@@ -1,103 +1,118 @@
 (ns publicator.domain.aggregate
+  (:refer-clojure :exclude [type])
   (:require
    [publicator.domain.abstractions.instant :as instant]
    [publicator.utils.datascript.validation :as d.validation]
-   [datascript.core :as d]))
+   [datascript.core :as d]
+   [clojure.set :as set]))
 
-(defn root [aggregate]
-  (d/entity aggregate :root))
+(defn root [agg]
+  (d/entity agg :root))
+
+(defn id [agg]
+  (-> agg root :root/id))
+
+(defn type [agg]
+  (-> agg meta ::spec :type))
 
 (def ^{:arglists '([query & inputs])} q d/q)
 
 (def root-q '{:find [[?e ...]]
               :where [[?e :db/ident :root]]})
 
-(def base-spec
-  {:schema        {:root/id {:db/unique :db.unique/identity}}
-   :defaults-tx   (fn [] [[:db/add 1 :db/ident :root]
-                          [:db/add :root :root/created-at (instant/*now*)]])
-   :additional-tx (fn [] [[:db/add :root :root/updated-at (instant/*now*)]])
-   :validator     (d.validation/compose
+#_(def base-spec
+    {:schema        {:root/id {:db/unique :db.unique/identity}}
+     :defaults-tx   (fn [] [[:db/add 1 :db/ident :root]
+                            [:db/add :root :root/created-at (instant/*now*)]])
+     :additional-tx (fn [] [[:db/add :root :root/updated-at (instant/*now*)]])
+     :validator     (d.validation/compose
 
-                   (d.validation/predicate
-                    [[:root/id         pos-int?]
-                     [:root/created-at inst?]
-                     [:root/updated-at inst?]])
+                     (d.validation/predicate
+                      [[:root/id         pos-int?]
+                       [:root/created-at inst?]
+                       [:root/updated-at inst?]])
 
-                   (d.validation/required
-                    root-q #{:root/id :root/created-at :root/updated-at})
+                     (d.validation/required
+                      root-q #{:root/id :root/created-at :root/updated-at})
 
-                   (d.validation/read-only
-                    root-q #{:root/id :root/created-at}))})
+                     (d.validation/read-only
+                      root-q #{:root/id :root/created-at}))})
 
-(defn extend-spec [spec other]
-  (cond-> spec
-    (contains? other :type)
-    (assoc :type (:type other))
+#_(defn extend-spec [spec other]
+    (cond-> spec
+      (contains? other :type)
+      (assoc :type (:type other))
 
-    :always
-    (update :schema merge (:schema other))
+      :always
+      (update :schema merge (:schema other))
 
-    (not (contains? spec :defaults-tx))
-    (assoc :defaults-tx (fn [] []))
+      (not (contains? spec :defaults-tx))
+      (assoc :defaults-tx (fn [] []))
 
-    (contains? other :defaults-tx)
-    (update :defaults-tx (fn [old] #(concat (old) ((:defaults-tx other)))))
+      (contains? other :defaults-tx)
+      (update :defaults-tx (fn [old] #(concat (old) ((:defaults-tx other)))))
 
-    (not (contains? spec :additional-tx))
-    (assoc :additional-tx (fn [] []))
+      (not (contains? spec :additional-tx))
+      (assoc :additional-tx (fn [] []))
 
-    (contains? other :additional-tx)
-    (update :additional-tx (fn [old] #(concat (old) ((:additional-tx other)))))
+      (contains? other :additional-tx)
+      (update :additional-tx (fn [old] #(concat (old) ((:additional-tx other)))))
 
-    (not (contains? spec :validator))
-    (assoc :validator d.validation/null-validator)
+      (not (contains? spec :validator))
+      (assoc :validator d.validation/null-validator)
 
-    (contains? other :validator)
-    (update :validator d.validation/compose (:validator other))))
+      (contains? other :validator)
+      (update :validator d.validation/compose (:validator other))))
 
-(defn extend-agg [agg spec]
-  (vary-meta agg update :aggregate/spec extend-spec spec))
+#_(defn- check-errors! [agg]
+    (let [errs (-> agg meta :aggregate/errors)]
+      (if (not-empty errs)
+        (throw (ex-info "Aggregate has errors" {:type   ::has-errors
+                                                :errors errs}))
+        agg)))
 
-(defn- check-errors! [agg]
-  (let [errs (-> agg meta :aggregate/errors)]
-    (if (not-empty errs)
-      (throw (ex-info "Aggregate has errors" {:type   ::has-errors
-                                              :errors errs}))
-      agg)))
+(defn build [spec]
+  (let [schema       (:schema spec)
+        id-generator (:id-generator spec)
+        tx           (cond-> []
+                       :always             (conj [:db/add 1 :db/ident :root])
+                       :always             (conj [:db/add :root :root/created-at (instant/*now*)])
+                       (ifn? id-generator) (conj [:db/add :root :root/id (id-generator)]))]
+    (-> (d/empty-db schema)
+        (d/db-with tx)
+        (vary-meta assoc ::spec spec))))
 
-(defn build [spec tx-data]
-  (let [spec    (extend-spec base-spec spec)
-        tx-data (concat ((:defaults-tx spec))
-                        tx-data
-                        ((:additional-tx spec)))
-        agg     (d/empty-db (:schema spec))
-        report  (d/with agg tx-data)
-        agg     (:db-after report)
-        tx-data (:tx-data report)
-        errors  (d.validation/validate report (:validator spec))
-        agg     (vary-meta agg merge {:type              (:type spec)
-                                      :aggregate/spec    spec
-                                      :aggregate/tx-data tx-data
-                                      :aggregate/errors  errors})]
-    agg))
+(def allow-everething
+  (fn [report] nil))
 
-(defn build! [spec tx-data]
-  (-> (build spec tx-data)
-      (check-errors!)))
+(defn allow-attributes [attrs]
+  (fn [{:keys [tx-data] :as report}]
+    (let [attrs     (set attrs)
+          all-attrs (->> tx-data
+                         (map (fn [[e a v tx added]] a))
+                         (set))
+          denied    (set/difference all-attrs attrs)]
+      (if (not-empty denied)
+        (throw (ex-info "Wrong transaction"
+                        {:type    ::denied-attributes
+                         :denied  denied
+                         :allowed attrs
+                         :report  report}))))))
 
-(defn change [agg tx-data]
-  (let [spec    (-> agg meta :aggregate/spec)
-        tx-data (concat tx-data
-                        ((:additional-tx spec)))
-        report  (d/with agg tx-data)
-        agg     (:db-after report)
-        tx-data (:tx-data report)
-        errors  (d.validation/validate report (:validator spec))
-        agg     (vary-meta agg merge {:aggregate/tx-data tx-data
-                                      :aggregate/errors  errors})]
-    agg))
 
-(defn change! [agg tx-data]
-  (-> (change agg tx-data)
-      (check-errors!)))
+(defn change [agg tx-data tx-validator!]
+  (let [report (d/with agg tx-data)]
+    (tx-validator! report)
+    (:db-after report)))
+
+;; (defn change [agg tx-data]
+;;   (let [spec    (-> agg meta :aggregate/spec)
+;;         tx-data (concat tx-data
+;;                         ((:additional-tx spec)))
+;;         report  (d/with agg tx-data)
+;;         agg     (:db-after report)
+;;         tx-data (:tx-data report)
+;;         errors  (d.validation/validate report (:validator spec))
+;;         agg     (vary-meta agg merge {:aggregate/tx-data tx-data
+;;                                       :aggregate/errors  errors})]
+;;     agg))
--- core/test/publicator/domain/aggregate_test.clj
+++ core/test/publicator/domain/aggregate_test.clj
@@ -3,7 +3,6 @@
    [publicator.domain.aggregate :as agg]
    [publicator.domain.abstractions.test-impl.scaffolding :as scaffolding]
    [publicator.utils.datascript.fns :as d.fns]
-   [publicator.utils.datascript.validation :as d.validation]
    [clojure.test :as t]))
 
 (t/use-fixtures :each scaffolding/setup)
@@ -11,84 +10,37 @@
 (def ^:const id 42)
 
 (def spec
-  {:type          :test-agg
-   :schema        {:inner/base {:db/valueType :db.type/ref}}
-   :defaults-tx   (fn [] [[:db/add :root :root/id id]
-                          [:db/add :root :test-agg/version 0]
-                          [:db/add :root :test-agg/read-only 0]])
-   :additional-tx (fn [] [[:db.fn/call d.fns/update-all :test-agg/version inc]])
-   :validator     (d.validation/compose
-
-                   (d.validation/predicate
-                    [[:test-agg/key keyword?]
-                     [:test-agg/version pos-int?]
-                     [:teat-agg/read-only pos-int?]
-                     [:inner/key keyword?]])
-
-                   (d.validation/read-only #{:test-agg/read-only}))})
+  {:type         :test-agg
+   :id-generator (constantly id)
+   :validator    (fn [_])})
 
 (t/deftest build
-  (t/testing "main path"
-    (let [agg (agg/build spec
-                         [[:db/add :root :test-agg/key :val]
-                          [:db/add "inner" :inner/base :root]
-                          [:db/add "inner" :inner/key :inner-val]])]
-      (t/testing "some"
-        (t/is (some? agg)))
-      (t/testing "id"
-        (t/is (= id (-> agg agg/root :root/id))))
-      (t/testing "type"
-        (t/is (= :test-agg (type agg))))
-      (t/testing :schema
-        (t/is (= :inner-val (-> agg agg/root :inner/_base first :inner/key))))
-      (t/testing :additional-tx
-        (t/is (= 1 (-> agg agg/root :test-agg/version))))
-      (t/testing :aggregate/tx-data
-        (t/is (not-empty (-> agg meta :aggregate/tx-data))))
-      (t/testing :aggregate/errors
-        (t/is (empty? (-> agg meta :aggregate/errors))))))
-  (t/testing "errors"
-    (let [agg (agg/build spec [[:db/add :root :test-agg/key "wrong"]])]
-      (t/is (not-empty (-> agg meta :aggregate/errors))))))
-
-(t/deftest build!
-  (t/testing "main path"
-    (t/is (some? (agg/build! spec [[:db/add :root :test-agg/key :new-val]]))))
-  (t/testing "errors"
-    (t/is (thrown-with-msg? clojure.lang.ExceptionInfo
-                            #"Aggregate has errors"
-                            (agg/build! spec [[:db/add :root :test-agg/key "wrong"]])))))
+  (let [agg (agg/build spec)]
+    (t/testing "some"
+      (t/is (some? agg)))
+    (t/testing "id"
+      (t/is (= id (agg/id agg))))
+    (t/testing "type"
+      (t/is (= :test-agg (agg/type agg))))
+    (t/testing "spec"
+      (t/is (= spec (-> agg meta ::agg/spec))))))
 
 (t/deftest change
-  (let [agg (agg/build! spec
-                        [[:db/add :root :test-agg/key :val]])]
-    (t/testing "main path"
-      (let [agg (agg/change agg [[:db/add :root :test-agg/key :new-val]])]
-        (t/testing "update"
-          (t/is (= :new-val (-> agg agg/root :test-agg/key))))
-        (t/testing :additional-tx
-          (t/is (= 2 (-> agg agg/root :test-agg/version))))
-        (t/testing :aggregate/tx-data
-          (t/is (= [[:test-agg/key false]
-                    [:test-agg/key true]
-                    [:root/updated-at false]
-                    [:root/updated-at true]
-                    [:test-agg/version false]
-                    [:test-agg/version true]]
-                   (map (fn [[e a v t added]] [a added])
-                        (-> agg meta :aggregate/tx-data)))))
-        (t/testing :aggregate/errors
-          (t/is (empty? (-> agg meta :aggregate/errors))))))
-    (t/testing "errors"
-      (let [agg (agg/change agg [[:db/add :root :test-agg/key "wrong"]])]
-        (t/is (not-empty (-> agg meta :aggregate/errors)))))))
-
-(t/deftest change!
-  (let [agg (agg/build! spec
-                        [[:db/add :root :test-agg/key :val]])]
-    (t/testing "main path"
-      (t/is (some? (agg/change! agg [[:db/add :root :test-agg/key :new-val]]))))
-    (t/testing "errors"
-      (t/is (thrown-with-msg? clojure.lang.ExceptionInfo
-                              #"Aggregate has errors"
-                              (agg/change! agg [[:db/add :root :test-agg/key "wrong"]]))))))
+  (let [agg (agg/build spec)]
+    (t/testing "allow-everething"
+      (let [agg (agg/change agg
+                            [[:db/add :root :test-agg/key :val]]
+                            agg/allow-everething)]
+        (t/is (= :val (-> agg agg/root :test-agg/key)))))
+    (t/testing "allow-attributes"
+      (t/testing "allowed"
+        (let [agg (agg/change agg
+                              [[:db/add :root :test-agg/key :val]]
+                              (agg/allow-attributes #{:test-agg/key}))]
+          (t/is (= :val (-> agg agg/root :test-agg/key)))))
+      (t/testing "rejected"
+        (t/is (thrown? clojure.lang.ExceptionInfo
+                       #"Wrong transaction"
+                       (agg/change agg
+                                   [[:db/add :root :wrong-attr :val]]
+                                   (agg/allow-attributes #{:test-agg/key}))))))))
