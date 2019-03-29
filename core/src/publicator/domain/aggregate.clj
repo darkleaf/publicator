26d94c2e04c92a646850c9a98e5c6493f4d433ff
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-03-29 21:27:15 +0400

    fix

--- core/src/publicator/domain/aggregate.clj
+++ core/src/publicator/domain/aggregate.clj
@@ -20,63 +20,11 @@
 (def root-q '{:find [[?e ...]]
               :where [[?e :db/ident :root]]})
 
-#_(def base-spec
-    {:schema        {:root/id {:db/unique :db.unique/identity}}
-     :defaults-tx   (fn [] [[:db/add 1 :db/ident :root]
-                            [:db/add :root :root/created-at (instant/*now*)]])
-     :additional-tx (fn [] [[:db/add :root :root/updated-at (instant/*now*)]])
-     :validator     (d.validation/compose
-
-                     (d.validation/predicate
-                      [[:root/id         pos-int?]
-                       [:root/created-at inst?]
-                       [:root/updated-at inst?]])
-
-                     (d.validation/required
-                      root-q #{:root/id :root/created-at :root/updated-at})
-
-                     (d.validation/read-only
-                      root-q #{:root/id :root/created-at}))})
-
-#_(defn extend-spec [spec other]
-    (cond-> spec
-      (contains? other :type)
-      (assoc :type (:type other))
-
-      :always
-      (update :schema merge (:schema other))
-
-      (not (contains? spec :defaults-tx))
-      (assoc :defaults-tx (fn [] []))
-
-      (contains? other :defaults-tx)
-      (update :defaults-tx (fn [old] #(concat (old) ((:defaults-tx other)))))
-
-      (not (contains? spec :additional-tx))
-      (assoc :additional-tx (fn [] []))
-
-      (contains? other :additional-tx)
-      (update :additional-tx (fn [old] #(concat (old) ((:additional-tx other)))))
-
-      (not (contains? spec :validator))
-      (assoc :validator d.validation/null-validator)
-
-      (contains? other :validator)
-      (update :validator d.validation/compose (:validator other))))
-
-#_(defn- check-errors! [agg]
-    (let [errs (-> agg meta :aggregate/errors)]
-      (if (not-empty errs)
-        (throw (ex-info "Aggregate has errors" {:type   ::has-errors
-                                                :errors errs}))
-        agg)))
-
 (defn build [spec]
   (let [schema       (:schema spec)
         id-generator (:id-generator spec)
         tx           (cond-> []
                        :always             (conj [:db/add 1 :db/ident :root])
-                       :always             (conj [:db/add :root :root/created-at (instant/*now*)])
                        (ifn? id-generator) (conj [:db/add :root :root/id (id-generator)]))]
     (-> (d/empty-db schema)
         (d/db-with tx)
@@ -99,20 +47,7 @@
                          :allowed attrs
                          :report  report}))))))
 
-
-(defn change [agg tx-data tx-validator!]
+(defn change [agg tx-data tx-validator]
   (let [report (d/with agg tx-data)]
-    (tx-validator! report)
+    (tx-validator report)
     (:db-after report)))
-
-;; (defn change [agg tx-data]
-;;   (let [spec    (-> agg meta :aggregate/spec)
-;;         tx-data (concat tx-data
-;;                         ((:additional-tx spec)))
-;;         report  (d/with agg tx-data)
-;;         agg     (:db-after report)
-;;         tx-data (:tx-data report)
-;;         errors  (d.validation/validate report (:validator spec))
-;;         agg     (vary-meta agg merge {:aggregate/tx-data tx-data
-;;                                       :aggregate/errors  errors})]
-;;     agg))
