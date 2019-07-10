2a6b2b21b7310ed3914157605dc4a66c2dafd8d4
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-07-10 22:07:34 +0400

    save

--- core/src/publicator/domain/aggregate.clj
+++ core/src/publicator/domain/aggregate.clj
@@ -1,43 +1,106 @@
 (ns publicator.domain.aggregate
   (:require
-   [datascript.core :as d]))
+   [datascript.core :as d]
+   [datascript.parser :as d.p]
+   [clojure.core.match :as m]))
 
-(defn rules [agg]
-  (-> agg meta ::rules))
-
-;; (defn validate [agg]
-;;   ((-> agg meta ::validate) agg))
+(defprotocol Aggregate
+  :extend-via-metadata true
+  (rules [agg])
+  (validators [agg])
+  (msg->tx [agg msg]))
 
 (defn extend-schema [agg ext]
-  (let [schema (merge ext (:schema agg))
-        datoms (d/datoms agg :eavt)]
-    (d/init-db datoms schema)))
+  (let [schema   (merge ext (:schema agg))
+        metadata (meta agg)]
+    (-> agg
+        (d/datoms :eavt)
+        (d/init-db schema)
+        (with-meta metadata))))
 
 (defn decorate [agg decorators-map]
-  (let [merge-fn (fn [val decorator] (decorator val))]
-    (vary-meta agg #(merge-with merge-fn % decorators-map))))
+  (vary-meta agg #(merge-with partial decorators-map %)))
+
+(defn- rules-impl [agg]
+  '[[(root ?e)
+     [?e :db/ident :root]]])
+
+(defn- validators-impl [agg]
+  [])
+
+(defn- msg->tx-impl [agg msg]
+  (m/match msg
+    {:type   :add-attr
+     :entity e
+     :attr   a
+     :value  v}
+    [[:db/add e a v]]))
 
 (def blank
   (-> (d/empty-db)
       (d/db-with [[:db/add 1 :db/ident :root]])
       (with-meta
-        {::validate identity
-         ::rules    '[[(root ?e)
-                       [?e :db/ident :root]]]})))
-
-(defmulti reducer (fn [agg [kind & rest]] kind))
+        {`rules      #'rules-impl
+         `validators #'validators-impl
+         `msg->tx    #'msg->tx-impl})))
 
 (defn root [agg]
   (d/entity agg :root))
 
-(def ^{:arglists '([agg tx-data])} agg-with d/db-with)
+(def ^{:arglists '([agg tx-data])} with d/db-with)
 
 (defn q
   ([agg query] (q agg query []))
   ([agg query inputs]
-   (let [query  (update query :in (fn [in] (concat '[$ %] in)))
+   (let [query  (cond
+                  (map? query) query
+                  (sequential? query) (d.p/query->map query))
+         query  (update query :in (fn [in] (concat '[$ %] in)))
          inputs (concat [agg (rules agg)] inputs)]
      (apply d/q query inputs))))
 
-(defmethod reducer :agg/add-attribute [agg [attr {:keys [entity value]}]]
-  (agg-with agg [[:db/add entity attr value]]))
+(defn apply-msg [agg msg]
+  (with agg (msg->tx agg msg)))
+
+(defn has-errors? [agg]
+  (not-empty
+   (q agg '{:find [[?e ...]]
+            :where [[?e :error/type _]]})))
+
+(defn validate [agg]
+  (reduce
+   (fn [agg validator]
+     (if (has-errors? agg)
+       (reduced agg)
+       (with agg (validator agg))))
+   agg
+   (validators agg)))
+
+(defn required-validator [agg->entities attrs]
+  (fn [agg]
+    (let [entities (agg->entities agg)
+          data     (q agg
+                      '{:find  [?e ?a]
+                        :in    [[?e ...] [?a ...]]
+                        :where [[(missing? $ ?e ?a)]]}
+                      [entities attrs])]
+      (for [[e a] data]
+        {:error/type   :required
+         :error/entity e
+         :error/attr   a}))))
+
+(defn predicate-validator [agg->entities pred-map]
+  (fn [agg]
+    (let [entities (agg->entities agg)
+          data     (q agg
+                      '{:find  [?e ?a ?v ?pred]
+                        :in    [?apply [?e ...] [[?a ?pred]]]
+                        :where [[?e ?a ?v]
+                                (not [(?apply ?pred ?v [])])]}
+                      [apply entities pred-map])]
+      (for [[e a v pred] data]
+        {:error/type   :predicate
+         :error/entity e
+         :error/attr   a
+         :error/value  v
+         :error/pred   pred}))))
--- core/test/publicator/domain/aggregate_test.clj
+++ core/test/publicator/domain/aggregate_test.clj
@@ -3,10 +3,9 @@
    [publicator.domain.aggregate :as agg]
    [clojure.test :as t]))
 
-(derive :test-agg/attr :agg/add-attribute)
-
 (t/deftest blank
   (t/is (some? agg/blank))
+  (t/is (not (agg/has-errors? agg/blank)))
   (t/is (some? (agg/root agg/blank)))
   (t/is (= 1 (agg/q agg/blank '{:find [?e .], :where [(root ?e)]}))))
 
@@ -14,35 +13,78 @@
   (let [agg (-> agg/blank
                 (agg/extend-schema {:test-agg/many {:db/cardinality :db.cardinality/many}}))]
     (t/is (some? (agg/root agg)))
-    (t/is (-> agg :schema (contains? :test-agg/many)))))
+    (t/is (-> agg :schema (contains? :test-agg/many)))
+    (t/is (= (meta agg/blank)
+             (meta agg)))))
 
 (t/deftest decorate
-  (let [agg (agg/decorate agg/blank
-                          {::agg/rules (fn [rules]
-                                         (conj rules
-                                               '[(attr ?v)
-                                                 [:root :test-agg/attr ?v]]))})]
+  (let [decorators {`agg/rules (fn [super agg]
+                                 (conj (super agg)
+                                       '[(attr ?v)
+                                         [:root :test-agg/attr ?v]]))}
+        agg        (agg/decorate agg/blank decorators)]
     (t/is (= 'attr (-> agg agg/rules last first first)))))
-
-(t/deftest reducer
-  (let [agg (agg/reducer agg/blank [:test-agg/attr {:entity :root, :value :foo}])]
-    (t/is (= :foo (-> agg agg/root :test-agg/attr)))))
 
 (t/deftest q
   (t/testing "rules"
-    (let [agg (-> agg/blank
-                  (agg/decorate {::agg/rules (fn [rules]
-                                               (conj rules
-                                                     '[(attr ?v)
-                                                       [:root :test-agg/attr ?v]]))})
-                  (agg/reducer [:test-agg/attr {:entity :root, :value :foo}]))]
-      (t/is (= :foo (agg/q agg '{:find [?v .]
+    (let [rules-d    (fn [super agg]
+                       (conj (super agg)
+                             '[(attr ?v)
+                               [:root :test-agg/attr ?v]]))
+          decorators {`agg/rules rules-d}
+          agg        (-> agg/blank
+                         (agg/decorate decorators)
+                         (agg/with [[:db/add :root :test-agg/attr :foo]]))]
+      (t/is (= :foo (agg/q agg '{:find  [?v .]
                                  :where [(attr ?v)]})))))
   (t/testing "bindings"
-    (let [agg (-> agg/blank
-                  (agg/reducer [:test-agg/attr {:entity :root, :value :foo}]))]
+    (let [agg (agg/with agg/blank [[:db/add :root :test-agg/attr :foo]])]
       (t/is (= :foo (agg/q agg
                            '{:find  [?v .]
-                             :in [?attr]
+                             :in    [?attr]
                              :where [[:root ?attr ?v]]}
                            [:test-agg/attr]))))))
+
+(t/deftest apply-msg
+  (let [msg {:type   :add-attr
+             :entity :root
+             :attr   :test-agg/attr
+             :value  :foo}
+        agg (agg/apply-msg agg/blank msg)]
+    (t/is (= :foo (-> agg agg/root :test-agg/attr)))))
+
+(t/deftest validate
+  (t/testing "generic"
+    (let [validators-d (fn [super agg]
+                         (conj (super agg)
+                               (fn [agg]
+                                 [{:error/enitiy 1
+                                   :error/type   :generic}])))
+          decorators   {`agg/validators validators-d}
+          agg          (-> agg/blank
+                           (agg/decorate decorators)
+                           (agg/validate))]
+      (t/is (agg/has-errors? agg))))
+  (t/testing "required"
+    (let [validators-d (fn [super agg]
+                         (conj (super agg)
+                               (agg/required-validator
+                                #(agg/q % '[:find [?e ...] :where (root ?e)])
+                                #{:test-att/attr})))
+          decorators   {`agg/validators validators-d}
+          agg          (-> agg/blank
+                           (agg/decorate decorators)
+                           (agg/validate))]
+      (t/is (agg/has-errors? agg))))
+  (t/testing "predicate"
+    (let [validators-d (fn [super agg]
+                         (conj (super agg)
+                               (agg/predicate-validator
+                                #(agg/q % '[:find [?e ...] :where (root ?e)])
+                                {:test-agg/attr int?})))
+          decorators   {`agg/validators validators-d}
+          agg          (-> agg/blank
+                           (agg/with [[:db/add :root :test-agg/attr :wrong]])
+                           (agg/decorate decorators)
+                           (agg/validate))]
+      (t/is (agg/has-errors? agg)))))
