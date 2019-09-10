d895f014a6ed181df16f115314f93cfd14b31b98
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-09-10 14:57:18 +0400

    fix script

--- core/test/publicator/use_cases/user/register2_test.clj
+++ core/test/publicator/use_cases/user/register2_test.clj
@@ -7,66 +7,78 @@
    [clojure.core.match :as m]
    [clojure.test :as t]))
 
-(defn- check-with-script [script fn & args]
-  (loop [[effect callback]          (apply fn args)
-         [[expected result] & s-tail] script]
-    (t/is (= expected effect))
-    (if (nil? callback)
-      (t/is (empty? s-tail))
-      (recur (callback result)
-             s-tail))))
+(defn- check-with-script [continuation script]
+  (loop [[actual-effect continuation]       [nil continuation]
+         [{:keys [effect coeffect]} & tail] script]
+    (t/is (= effect actual-effect))
+    (if (nil? continuation)
+      (t/is (empty? tail))
+      (recur (continuation coeffect)
+             tail))))
 
 (t/deftest process-success
   (let [msgs   [[:user/login :add :root "john"]
                 [:user/password :add :root "password"]]
-        script [[[:get-session] {}]
-                [[:get-user-presence-by-login "john"] false]
-                [[:get-password-digest "password"] "digest"]
-                [[:get-new-user-id] 1]
-                [[:do
-                  [:set-session {:current-user-id 1}]
-                  [:persist (-> user/new-blank
-                                (agg/with-msgs msgs)
-                                (agg/with-msgs
-                                  [[:agg/id :add :root 1]
-                                   [:user/password-digest :add :root "digest"]
-                                   [:user/state :add :root :active]]))]]
-                 [nil nil]]
-                [[:show-screen :main]]]]
-    (check-with-script script register/process msgs)))
+        script [{:coeffect msgs}
+                {:effect   [:get-session]
+                 :coeffect {}}
+                {:effect   [:get-user-presence-by-login "john"]
+                 :coeffect false}
+                {:effect   [:get-password-digest "password"]
+                 :coeffect "digest"}
+                {:effect   [:get-new-user-id]
+                 :coeffect 1}
+                {:effect   [:do
+                            [:set-session {:current-user-id 1}]
+                            [:persist (-> user/new-blank
+                                          (agg/with-msgs msgs)
+                                          (agg/with-msgs
+                                            [[:agg/id :add :root 1]
+                                             [:user/password-digest :add :root "digest"]
+                                             [:user/state :add :root :active]]))]]
+                 :coeffect [nil nil]}
+                {:effect [:show-screen :main]}]]
+    (check-with-script register/process script)))
 
 (t/deftest process-additional-msgs
-  (let [msgs   [[:user/login :add :root "john"]
-                [:user/password :add :root "password"]
-                [:user/state :add :root :archived]]
-        script [[[:show-additional-messages-error #{:user/state}]]]]
-    (check-with-script script register/process msgs)))
+  (let [script [{:coeffect [[:user/login :add :root "john"]
+                            [:user/password :add :root "password"]
+                            [:user/state :add :root :archived]]}
+                {:effect [:show-additional-messages-error #{:user/state}]}]]
+    (check-with-script register/process script)))
 
 (t/deftest process-already-logged-in
-  (let [msgs   [[:user/login :add :root "john"]
-                [:user/password :add :root "password"]]
-        script [[[:get-session] {:current-user-id 1}]
-                [[:show-screen :main]]]]
-    (check-with-script script register/process msgs)))
+  (let [script [{:coeffect [[:user/login :add :root "john"]
+                            [:user/password :add :root "password"]]}
+                {:effect   [:get-session]
+                 :coeffect {:current-user-id 1}}
+                {:effect [:show-screen :main]}]]
+    (check-with-script register/process script)))
 
 (t/deftest process-already-registered
-  (let [msgs   [[:user/login :add :root "john"]
-                [:user/password :add :root "password"]]
-        script [[[:get-session] {}]
-                [[:get-user-presence-by-login "john"] true]
-                [[:show-screen :main]]]]
-    (check-with-script script register/process msgs)))
+  (let [script [{:coeffect [[:user/login :add :root "john"]
+                            [:user/password :add :root "password"]]}
+
+                {:effect   [:get-session]
+                 :coeffect {}}
+                {:effect   [:get-user-presence-by-login "john"]
+                 :coeffect true}
+                {:effect [:show-screen :main]}]]
+    (check-with-script register/process script)))
 
 (t/deftest process-with-errr
-  (let [msgs   [[:user/login :add :root "john"]
-                [:user/password :add :root ""]]
-        script [[[:get-session] {}]
-                [[:get-user-presence-by-login "john"] false]
-                [[:get-password-digest ""] "digest"]
-                [[:show-validation-errors #{{:error/type   :predicate
-                                             :error/entity 1
-                                             :error/attr   :user/password
-                                             :error/value  ""
-                                             :error/pred   ".{8,255}"
-                                             :error/rule   'root}}]]]]
-    (check-with-script script register/process msgs)))
+  (let [script [{:coeffect [[:user/login :add :root "john"]
+                            [:user/password :add :root ""]]}
+                {:effect   [:get-session]
+                 :coeffect {}}
+                {:effect   [:get-user-presence-by-login "john"]
+                 :coeffect false}
+                {:effect   [:get-password-digest ""]
+                 :coeffect "digest"}
+                {:effect [:show-validation-errors #{{:error/type   :predicate
+                                                     :error/entity 1
+                                                     :error/attr   :user/password
+                                                     :error/value  ""
+                                                     :error/pred   ".{8,255}"
+                                                     :error/rule   'root}}]}]]
+    (check-with-script register/process script)))
