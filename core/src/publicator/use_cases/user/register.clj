1599a16e57064ded11a1b6a3b2f0d18d0b1006c5
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-09-18 23:31:04 +0400

    register

--- core/src/publicator/use_cases/user/register.clj
+++ core/src/publicator/use_cases/user/register.clj
@@ -1,51 +1,70 @@
 (ns publicator.use-cases.user.register
   (:require
-   [publicator.domain.aggregates.user :as user]
    [publicator.domain.aggregate :as agg]
-   [publicator.util :refer [<<- or-some]]))
+   [publicator.util :as u]))
 
 (def allowed-msgs #{:user/login :user/password})
 
-(defn- already-logged-in [session]
-  (when (-> session :current-user-id some?)
-    {:reaction {:type :show-screen
-                :name :main}}))
-
-(defn- has-additional-messages [msgs]
-  (let [additional (->> msgs
-                        (map first)
+(defn- check-additional-attrs [datoms]
+  (let [additional (->> datoms
+                        (map :a)
                         (remove allowed-msgs)
                         (set))]
     (when (not-empty additional)
-      {:reaction {:type :show-additional-messages-error
-                  :msgs additional}})))
+      [[:show-additional-messages-error additional]])))
 
-(defn- ->user [msgs password->digest]
-  (-> user/new-blank
-      (agg/with-msgs msgs)
-      (agg/with-msgs [[:user/state :add :root :active]])
-      (user/fill-password-digest password->digest)))
+(defn- check-session [next]
+  (u/linearize
+   [[:get-session] (fn [session] <>)]
+   (if (-> session :current-user-id some?)
+     [[:show-screen :main]])
+   (next nil)))
 
-(defn- has-validation-errors [user]
+(defn- user-from-tx-data [tx-data next]
+  (let [report  (-> (agg/allocate :agg/user)
+                    (agg/with tx-data))
+        user    (:db-after report)
+        tx-data (:tx-data report)]
+    (or (check-additional-attrs tx-data)
+        (-> user
+            (agg/agg-with [[:db/add :root :user/state :active]])
+            (next)))))
+
+(defn- check-registration [user next]
+  (u/linearize
+   [[:get-user-presence-by-login (-> user agg/root :user/login)] (fn [presence] <>)]
+   (if presence
+     [[:show-screen :main]])
+   (next nil)))
+
+(defn- check-validation-errors [user]
   (let [errors (-> user agg/validate agg/errors)]
     (when (not-empty errors)
-      {:reaction {:type   :show-validation-errors
-                  :errors errors}})))
+      [[:show-validation-errors errors]])))
 
-(defn- fill-id [user new-user-ids]
-  (let [id (first new-user-ids)]
-    (agg/with-msgs user [[:agg/id :add :root id]])))
+(defn- fill-id [user next]
+  (u/linearize
+   [[:get-new-user-id] (fn [id] <>)]
+   (-> user
+       (agg/agg-with [[:db/add :root :agg/id id]])
+       (next))))
 
-(defn process [msgs session login->user-presence password->digest new-user-ids]
-  (<<-
-   (or-some (already-logged-in session))
-   (or-some (has-additional-messages msgs))
-   (let [user (->user msgs password->digest)])
-   (or-some (has-validation-errors user))
-   (let [user (fill-id user new-user-ids)
-         id   (-> user agg/root :agg/id)])
-   #_"TODO: use login->user-presence"
-   {:set-session (assoc session :current-user-id id)
-    :persist     [user]
-    :reaction    {:type :show-screen
-                  :name :main}}))
+(defn fill-password-digest [user next]
+  (u/linearize
+   [[:get-password-digest (-> user agg/root :user/password)] (fn [digest] <>)]
+   (-> user
+       (agg/agg-with [[:db/add :root :user/password-digest digest]])
+       (next))))
+
+(defn process [tx-data]
+  (u/linearize
+   (user-from-tx-data tx-data (fn [user] <>))
+   (check-session (fn [_] <>))
+   (check-registration user (fn [_] <>))
+   (fill-password-digest user (fn [user] <>))
+   (or (check-validation-errors user))
+   (fill-id user (fn [user] <>))
+   [[:do
+     [:assoc-session :current-user-id (-> user agg/root :agg/id)]
+     [:persist user]
+     [:show-screen :main]]]))
--- core/test/publicator/use_cases/user/register_test.clj
+++ core/test/publicator/use_cases/user/register_test.clj
@@ -1,80 +1,84 @@
 (ns publicator.use-cases.user.register-test
   (:require
    [publicator.use-cases.user.register :as register]
-   [publicator.domain.aggregates.user :as user]
    [publicator.domain.aggregate :as agg]
    [clojure.test :as t]))
 
+(defn- check-with-script [continuation script]
+  (loop [[actual-effect continuation]       [nil continuation]
+         [{:keys [effect coeffect]} & tail] script]
+    (t/is (= effect actual-effect))
+    (if (nil? continuation)
+      (t/is (empty? tail))
+      (recur (continuation coeffect)
+             tail))))
+
 (t/deftest process-success
-  (let [msgs                 [[:user/login :add :root "john"]
-                              [:user/password :add :root "password"]]
-        session              {}
-        pass->digest         {"password" "digest"}
-        login->user-presence {"john" false}
-        new-user-ids         [1]
-        effects              (register/process msgs
-                                               session
-                                               login->user-presence
-                                               pass->digest
-                                               new-user-ids)
-        expected-effects     {:set-session {:current-user-id 1}
-                              :persist     [(-> user/new-blank
-                                                (agg/with-msgs msgs)
-                                                (agg/with-msgs
-                                                  [[:agg/id :add :root 1]
-                                                   [:user/password-digest :add :root "digest"]
-                                                   [:user/state :add :root :active]]))]
-                              :reaction    {:type :show-screen
-                                            :name :main}}]
-    (t/is (= expected-effects effects))))
+  (let [tx-data [{:db/ident      :root
+                  :user/login    "john"
+                  :user/password "password"}]
+        script  [{:coeffect tx-data}
+                 {:effect   [:get-session]
+                  :coeffect {}}
+                 {:effect   [:get-user-presence-by-login "john"]
+                  :coeffect false}
+                 {:effect   [:get-password-digest "password"]
+                  :coeffect "digest"}
+                 {:effect   [:get-new-user-id]
+                  :coeffect 1}
+                 {:effect [:do
+                           [:assoc-session :current-user-id 1]
+                           [:persist (-> (agg/allocate :agg/new-user)
+                                         (agg/agg-with tx-data)
+                                         (agg/agg-with [{:db/ident             :root
+                                                         :agg/id               1
+                                                         :user/password-digest "digest"
+                                                         :user/state           :active}]))]
+                           [:show-screen :main]]}]]
+    (check-with-script register/process script)))
 
 (t/deftest process-additional-msgs
-  (let [msgs                 [[:user/login :add :root "john"]
-                              [:user/password :add :root "pass"]
-                              [:user/state :add :root :archived]]
-        session              {}
-        pass->digest         {"pass" "digest"}
-        login->user-presence {"john" false}
-        new-user-ids         [1]
-        effects              (register/process msgs
-                                               session
-                                               login->user-presence
-                                               pass->digest
-                                               new-user-ids)
-        expected-effects     {:reaction {:type :show-additional-messages-error
-                                         :msgs #{:user/state}}}]
-    (t/is (= expected-effects effects))))
-
-(t/deftest process-with-errr
-  (let [msgs                 [[:user/password :add :root "password"]]
-        session              {}
-        pass->digest         {"password" "digest"}
-        login->user-presence {"john" false}
-        new-user-ids         [1]
-        effects              (register/process msgs
-                                               session
-                                               login->user-presence
-                                               pass->digest
-                                               new-user-ids)
-        expected-effects     {:reaction {:type   :show-validation-errors
-                                         :errors #{{:error/type   :required
-                                                    :error/entity 1
-                                                    :error/attr   :user/login
-                                                    :error/rule   'root}}}}]
-    (t/is (= expected-effects effects))))
+  (let [script [{:coeffect [{:db/ident      :root
+                             :user/login    "john"
+                             :user/password "password"
+                             :user/state    :archived}]}
+                {:effect [:show-additional-messages-error #{:user/state}]}]]
+    (check-with-script register/process script)))
 
 (t/deftest process-already-logged-in
-  (let [msgs                 [[:user/login :add :root "john"]
-                              [:user/password :add :root "password"]]
-        session              {:current-user-id 1}
-        pass->digest         {"password" "digest"}
-        login->user-presence {"john" false}
-        new-user-ids         [2]
-        effects              (register/process msgs
-                                               session
-                                               login->user-presence
-                                               pass->digest
-                                               new-user-ids)
-        expected-effects     {:reaction {:type :show-screen
-                                         :name :main}}]
-    (t/is (= expected-effects effects))))
+  (let [script [{:coeffect [{:db/ident      :root
+                             :user/login    "john"
+                             :user/password "password"}]}
+                {:effect   [:get-session]
+                 :coeffect {:current-user-id 1}}
+                {:effect [:show-screen :main]}]]
+    (check-with-script register/process script)))
+
+(t/deftest process-already-registered
+  (let [script [{:coeffect [{:db/ident      :root
+                             :user/login    "john"
+                             :user/password "password"}]}
+                {:effect   [:get-session]
+                 :coeffect {}}
+                {:effect   [:get-user-presence-by-login "john"]
+                 :coeffect true}
+                {:effect [:show-screen :main]}]]
+    (check-with-script register/process script)))
+
+(t/deftest process-with-errr
+  (let [script [{:coeffect [{:db/ident      :root
+                             :user/login    "john"
+                             :user/password ""}]}
+                {:effect   [:get-session]
+                 :coeffect {}}
+                {:effect   [:get-user-presence-by-login "john"]
+                 :coeffect false}
+                {:effect   [:get-password-digest ""]
+                 :coeffect "digest"}
+                {:effect [:show-validation-errors #{{:error/type   :predicate
+                                                     :error/entity 1
+                                                     :error/attr   :user/password
+                                                     :error/value  ""
+                                                     :error/pred   ".{8,255}"
+                                                     :error/rule   'root}}]}]]
+    (check-with-script register/process script)))
