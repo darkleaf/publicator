04aef43fc201280190b5456f5aee4e9b6052eaa3
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2019-11-07 00:23:29 +0400

    darkleaf/effect

--- core/src/publicator/use_cases/user/register.clj
+++ core/src/publicator/use_cases/user/register.clj
@@ -1,17 +1,19 @@
 (ns publicator.use-cases.user.register
   (:require
    [publicator.domain.aggregate :as agg]
-   [publicator.util :as u]))
+   [publicator.util :as u]
+   [darkleaf.effect.core :refer [eff !]]))
 
 (def allowed-attrs #{:user/login :user/password})
 
 (defn- check-additional-attrs [datoms]
-  (let [additional (->> datoms
-                        (map :a)
-                        (remove allowed-attrs)
-                        (set))]
-    (when (not-empty additional)
-      [[:ui/show-additional-attributes-error additional]])))
+  (eff
+    (if-let [additional (->> datoms
+                             (map :a)
+                             (remove allowed-attrs)
+                             (set)
+                             (not-empty))]
+      (! [:ui/show-additional-attributes-error additional]))))
 
 (defn- fill-user-defaults [user]
   (agg/apply-tx user [{:db/ident   :root
@@ -19,48 +21,47 @@
                        :user/role  :regular}]))
 
 (defn- check-registration [user]
-  (u/linearize
-   (let [login (-> user agg/root :user/login)])
-   [[:persistence/user-presence-by-login login] (fn [presence] <>)]
-   (if presence
-     [[:ui/show-main-screen]])
-   [[:sub/return]]))
+  (eff
+    (let [login    (-> user agg/root :user/login)
+          presence (! [:persistence/user-presence-by-login login])]
+      (when presence
+        (! [:ui/show-main-screen])))))
 
 (defn- check-validation-errors [user]
-  (let [errors (-> user agg/validate agg/errors)]
-    (when (not-empty errors)
-      [[:ui/show-validation-errors errors]])))
+  (eff
+    (if-let [errors (-> user agg/validate agg/errors not-empty)]
+      (! [:ui/show-validation-errors errors]))))
 
 (defn- fill-id [user]
-  (u/linearize
-   [[:persistence/next-user-id] (fn [id] <>)]
-   [[:sub/return (agg/apply-tx user [[:db/add :root :agg/id id]])]]))
+  (eff
+    (let [id (! [:persistence/next-user-id])]
+      (agg/apply-tx user [[:db/add :root :agg/id id]]))))
 
 (defn- fill-password-digest [user]
-  (u/linearize
-   [[:hasher/derive (-> user agg/root :user/password)] (fn [digest] <>)]
-   [[:sub/return (agg/apply-tx user [[:db/add :root :user/password-digest digest]])]]))
+  (eff
+    (let [password (-> user agg/root :user/password)
+          digest   (! [:hasher/derive password])]
+      (agg/apply-tx user [[:db/add :root :user/password-digest digest]]))))
 
-(defn precondition [_]
-  (u/linearize
-   [[:session/get] (fn [session] <>)]
-   (if (-> session :current-user-id some?)
-     [[:sub/return [[:ui/show-main-screen]]]]
-     [[:sub/return]])))
+(defn precondition []
+  (eff
+    (when (-> (! [:session/get])
+              :current-user-id
+              some?)
+      [:ui/show-main-screen])))
 
 (defn process [tx-data]
-  (u/linearize
-   (u/sub precondition nil (fn [ex-effect] <>))
-   (or ex-effect)
-   (let [[user datoms] (-> :agg/user agg/allocate (agg/apply-tx* tx-data))])
-   (or (check-additional-attrs datoms))
-   (let [user (fill-user-defaults user)])
-   (u/sub check-registration user (fn [_] <>))
-   (u/sub fill-password-digest user (fn [user] <>))
-   (or (check-validation-errors user))
-   (u/sub fill-id user (fn [user] <>))
-   (let [id (-> user agg/root :agg/id)])
-   [[:do
-     [:session/assoc :current-user-id id]
-     [:persistence/save user]
-     [:ui/show-main-screen]]]))
+  (eff
+    (if-some [ex-effect (! (precondition))]
+      (! ex-effect)
+      (let [[user datoms] (-> :agg/user agg/allocate (agg/apply-tx* tx-data))
+            _             (! (check-additional-attrs datoms))
+            user          (! (fill-user-defaults user))
+            _             (! (check-registration user))
+            user          (! (fill-password-digest user))
+            _             (! (check-validation-errors user))
+            user          (! (fill-id user))
+            id            (-> user agg/root :agg/id)]
+        (! [:session/assoc :current-user-id id])
+        (! [:persistence/save user])
+        (! [:ui/show-main-screen])))))
--- core/test/publicator/use_cases/user/register_test.clj
+++ core/test/publicator/use_cases/user/register_test.clj
@@ -3,13 +3,14 @@
    [publicator.use-cases.user.register :as register]
    [publicator.domain.aggregate :as agg]
    [publicator.util :as u]
+   [darkleaf.effect.core :as e]
    [clojure.test :as t]))
 
 (t/deftest process-success
   (let [tx-data [{:db/ident      :root
                   :user/login    "john"
                   :user/password "password"}]
-        script  [{:coeffect tx-data}
+        script  [{:args [tx-data]}
                  {:effect   [:session/get]
                   :coeffect {}}
                  {:effect   [:persistence/user-presence-by-login "john"]
@@ -18,62 +19,65 @@
                   :coeffect "digest"}
                  {:effect   [:persistence/next-user-id]
                   :coeffect 1}
-                 {:effect [:do
-                           [:session/assoc :current-user-id 1]
-                           [:persistence/save (-> (agg/allocate :agg/new-user)
-                                                  (agg/apply-tx tx-data)
-                                                  (agg/apply-tx [{:db/ident             :root
-                                                                  :agg/id               1
-                                                                  :user/password-digest "digest"
-                                                                  :user/state           :active
-                                                                  :user/role            :regular}]))]
-                           [:ui/show-main-screen]]}]]
-    (u/test-with-script register/process script)))
+                 {:effect   [:session/assoc :current-user-id 1]
+                  :coeffect nil}
+                 {:effect   [:persistence/save (-> (agg/allocate :agg/new-user)
+                                                   (agg/apply-tx tx-data)
+                                                   (agg/apply-tx [{:db/ident             :root
+                                                                   :agg/id               1
+                                                                   :user/password-digest "digest"
+                                                                   :user/state           :active
+                                                                   :user/role            :regular}]))]
+                  :coeffect nil}
+                 {:effect   [:ui/show-main-screen]
+                  :coeffect nil}
+                 {:return nil}]]
+    (e/test register/process script)))
 
 (t/deftest process-additional-attrs
-  (let [script [{:coeffect [{:db/ident      :root
-                             :user/login    "john"
-                             :user/password "password"
-                             :user/state    :archived}]}
+  (let [script [{:args [[{:db/ident      :root
+                          :user/login    "john"
+                          :user/password "password"
+                          :user/state    :archived}]]}
                 {:effect   [:session/get]
                  :coeffect {}}
-                {:effect [:ui/show-additional-attributes-error #{:user/state}]}]]
-    (u/test-with-script register/process script)))
+                {:final-effect [:ui/show-additional-attributes-error #{:user/state}]}]]
+    (e/test register/process script)))
 
 (t/deftest process-already-logged-in
-  (let [script [{:coeffect [{:db/ident      :root
-                             :user/login    "john"
-                             :user/password "password"}]}
+  (let [script [{:args [[{:db/ident      :root
+                          :user/login    "john"
+                          :user/password "password"}]]}
                 {:effect   [:session/get]
                  :coeffect {:current-user-id 1}}
-                {:effect [:ui/show-main-screen]}]]
-    (u/test-with-script register/process script)))
+                {:final-effect [:ui/show-main-screen]}]]
+    (e/test register/process script)))
 
 (t/deftest process-already-registered
-  (let [script [{:coeffect [{:db/ident      :root
-                             :user/login    "john"
-                             :user/password "password"}]}
+  (let [script [{:args [[{:db/ident      :root
+                          :user/login    "john"
+                          :user/password "password"}]]}
                 {:effect   [:session/get]
                  :coeffect {}}
                 {:effect   [:persistence/user-presence-by-login "john"]
                  :coeffect true}
-                {:effect [:ui/show-main-screen]}]]
-    (u/test-with-script register/process script)))
+                {:final-effect [:ui/show-main-screen]}]]
+    (e/test register/process script)))
 
 (t/deftest process-with-errr
-  (let [script [{:coeffect [{:db/ident      :root
-                             :user/login    "john"
-                             :user/password ""}]}
+  (let [script [{:args [[{:db/ident      :root
+                          :user/login    "john"
+                          :user/password ""}]]}
                 {:effect   [:session/get]
                  :coeffect {}}
                 {:effect   [:persistence/user-presence-by-login "john"]
                  :coeffect false}
                 {:effect   [:hasher/derive ""]
                  :coeffect "digest"}
-                {:effect [:ui/show-validation-errors #{{:error/type   :predicate
-                                                        :error/entity 1
-                                                        :error/attr   :user/password
-                                                        :error/value  ""
-                                                        :error/pred   ".{8,255}"
-                                                        :error/rule   'root}}]}]]
-    (u/test-with-script register/process script)))
+                {:final-effect [:ui/show-validation-errors #{{:error/type   :predicate
+                                                              :error/entity 1
+                                                              :error/attr   :user/password
+                                                              :error/value  ""
+                                                              :error/pred   ".{8,255}"
+                                                              :error/rule   'root}}]}]]
+    (e/test register/process script)))
