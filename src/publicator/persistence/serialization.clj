b5b457352c0bfda6052b019167c099fcd71b67d9
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2020-11-24 20:38:56 +0400

    serialization

--- src/publicator/persistence/serialization.clj
+++ src/publicator/persistence/serialization.clj
@@ -1,39 +1,77 @@
 (ns publicator.persistence.serialization
   (:require
-   [clojure.string :as str]
    [datascript.core :as d]
+   [datascript.db :as d.db]
    [medley.core :as m]
-   [publicator.core.domain.aggregate :as agg]))
+   [publicator.core.domain.aggregate :as agg]
+   [publicator.utils :refer [<<-]]))
+
+(defn- parse-field [field]
+  (<<-
+   (if-some [[_ lang attr] (re-matches #"\A(.+?)\$(.+)" field)]
+     {:kind :translation
+      :lang (keyword lang)
+      :attr (keyword attr)})
+   (if-some [[_ attr] (re-matches #"\A#(.+)" field)]
+     {:kind :nested
+      :attr (keyword attr)})
+   {:kind :root
+    :attr (keyword field)}))
+
+(defn- root-attr->field [attr]
+  (str (symbol attr)))
+
+(defn- translation-attr->field [lang attr]
+  (str (symbol lang) "$" (symbol attr)))
+
+(defn- nested-attr->field [attr]
+  (str "#" (symbol attr)))
 
 (defn agg->row [agg]
-  (let [root   (->> (d/pull agg '[*] 1)
-                    (m/remove-keys #(= "db" (namespace %)))
-                    (m/map-keys #(str "r" %)))
-        conjv  (fnil conj [])
-        nested (reduce (fn [acc [e a v]]
-                         (-> acc
-                             (update (str "e" a) conjv e)
-                             (update (str "v" a) conjv v)))
-                       {}
-                       (d/seek-datoms agg :eavt 2))]
-    (merge root nested)))
+  (let [root         (->> (d/pull agg '[*] :root)
+                          (m/remove-keys #(= "db" (namespace %)))
+                          (m/map-keys root-attr->field))
+        translations (->> agg
+                          (d/q '[:find ?lang (pull ?t [*])
+                                 :where
+                                 [?t :translation/root :root]
+                                 [?t :translation/lang ?lang]])
+                          (map (fn [[lang attrs]]
+                                 (->> attrs
+                                      (m/remove-keys #(= "translation" (namespace %)))
+                                      (m/map-keys (partial translation-attr->field lang)))))
+                          (reduce merge))
+        nested       (->> agg
+                          (d/q '[:find ?a (aggregate ?vec ?e) (aggregate ?vec ?v)
+                                 :in ?vec $
+                                 :where
+                                 [?e ?a ?v]
+                                 (not [?e :db/ident :root])
+                                 (not [?e :translation/root :root])]
+                               vec)
+                          (reduce (fn [acc [a es vs]]
+                                    (assoc acc (nested-attr->field a) [es vs]))
+                                  {}))]
+    (merge root translations nested)))
 
 (defn row->agg [row]
-  (let [root-tag?   #(str/starts-with? % "r")
-        tag->attr    #(-> % (subs 2) (keyword))
-        root         (->> row
-                          (m/filter-keys root-tag?)
-                          (m/map-keys tag->attr)
-                          (merge {:db/id 1 :db/ident :root}))
-        nested-attrs (->> row
-                          keys
-                          (remove root-tag?)
-                          (map tag->attr)
-                          (set))
-        nested       (mapcat (fn [attr]
-                               (let [es (get row (str "e" attr))
-                                     vs (get row (str "v" attr))]
-                                 (map d/datom es (repeat attr) vs)))
-                             nested-attrs)]
-    (-> (agg/build)
-        (d/db-with (cons root nested)))))
+  (let [agg      (agg/build)
+        pair->tx (fn [[field data]]
+                   (let [{:keys [kind attr lang]} (parse-field field)]
+                     (case kind
+                       :root        [[:db/add :root attr data]]
+                       :translation (<<-
+                                     (let [id-field (translation-attr->field lang :db/id)
+                                           id       (get row id-field)])
+                                     (if (= :db/id attr)
+                                       [[:db/add id :translation/lang lang]
+                                        [:db/add id :translation/root :root]])
+                                     (if (d.db/multival? agg attr)
+                                       (for [v data]
+                                         [:db/add id attr v]))
+                                     [[:db/add id attr data]])
+                       :nested      (map (fn [e v]
+                                           [:db/add e attr v])
+                                         (first data) (second data)))))
+        tx       (mapcat pair->tx row)]
+    (d/db-with agg tx)))
--- test/publicator/persistence/serialization_test.clj
+++ test/publicator/persistence/serialization_test.clj
@@ -3,33 +3,39 @@
    [clojure.test :as t]
    [datascript.core :as d]
    [publicator.core.domain.aggregate :as agg]
+   [publicator.core.domain.aggregates.translation :as translation]
    [publicator.persistence.serialization :as sut]))
 
 (swap! agg/schema merge
-       {::ref  {:db/valueType :db.type/ref}
-        ::tags {:db/cardinality :db.cardinality/many}})
+       {:st/state             {:agg/predicate keyword?}
+        :st.translation/title {:agg/predicate string?}
+        :st.translation/tags  {:db/cardinality :db.cardinality/many}
+        :st.nested/root       {:db/valueType :db.type/ref}
+        :st.nested/attr-1     {:agg/predicate string?}
+        :st.nested/attr-2     {:agg/predicate int?}})
 
 (t/deftest ok
-  (let [agg (-> (agg/build)
-                (d/db-with [{:db/ident :root
-                             ::a       "x"
-                             ::tags    ["a" "b"]}
-                            {::a    "x"
-                             ::ref  :root
-                             ::tags ["a" "c"]}
-                            {::a    "y"
-                             ::b    "y"
-                             ::ref  :root
-                             ::tags ["c" "d"]}]))
-        row {"r:publicator.persistence.serialization-test/tags" ["a" "b"]
-             "r:publicator.persistence.serialization-test/a"    "x"
-             "e:publicator.persistence.serialization-test/a"    [2 3]
-             "v:publicator.persistence.serialization-test/a"    ["x" "y"]
-             "e:publicator.persistence.serialization-test/b"    [3]
-             "v:publicator.persistence.serialization-test/b"    ["y"]
-             "e:publicator.persistence.serialization-test/ref"  [2 3]
-             "v:publicator.persistence.serialization-test/ref"  [1 1]
-             "e:publicator.persistence.serialization-test/tags" [2 2 3 3]
-             "v:publicator.persistence.serialization-test/tags" ["a" "c" "c" "d"]}]
+  (let [agg (-> (agg/build {:db/ident :root
+                            :st/state :ok}
+                           {:translation/root     :root
+                            :translation/lang     :en
+                            :st.translation/title "title"
+                            :st.translation/tags  #{:tag-1 :tag-2}}
+                           {:st.nested/root   :root
+                            :st.nested/attr-1 "str-1"
+                            :st.nested/attr-2 1}
+                           {:st.nested/root   :root
+                            :st.nested/attr-1 "str-2"
+                            :st.nested/attr-2 2})
+                (agg/validate)
+                (translation/validate)
+                (agg/check-errors))
+        row {"st/state"                :ok
+             "en$db/id"                2
+             "en$st.translation/title" "title"
+             "en$st.translation/tags"  [:tag-1 :tag-2]
+             "#st.nested/root"          [[3 4] [1 1]]
+             "#st.nested/attr-1"        [[3 4] ["str-1" "str-2"]]
+             "#st.nested/attr-2"        [[3 4] [1 2]]}]
     (t/is (= row (sut/agg->row agg)))
     (t/is (= agg (sut/row->agg row)))))
