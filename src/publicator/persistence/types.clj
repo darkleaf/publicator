458e4c246dc0e0f6be3d0db7d3fb3c42ff9438c5
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2020-12-05 16:50:02 +0400

    pg_array

---
+++ src/publicator/persistence/types.clj
@@ -0,0 +1,18 @@
+(ns publicator.persistence.types
+  (:require
+   [next.jdbc.prepare :as prepare]
+   [next.jdbc.result-set :as result-set])
+  (:import
+   [java.sql PreparedStatement Array]))
+
+(set! *warn-on-reflection* true)
+
+(extend-protocol result-set/ReadableColumn
+  Array
+  (read-column-by-label [^Array v _]    (vec (.getArray v)))
+  (read-column-by-index [^Array v _ _]  (vec (.getArray v))))
+
+(extend-protocol prepare/SettableParameter
+  clojure.lang.IPersistentVector
+  (set-parameter [v ^PreparedStatement s i]
+    (.setObject s i (into-array v))))
