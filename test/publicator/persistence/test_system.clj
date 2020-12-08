ef0bcb86fedfeb8a9c980dc3145b7e0168e57487
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2020-12-08 20:45:40 +0400

    check contracts

--- test/publicator/persistence/test_system.clj
+++ test/publicator/persistence/test_system.clj
@@ -26,27 +26,16 @@
          (component/stop-system ~sys-name)))))
 
 (defn wrap [f*]
-  (-> f*
-      (e/wrap)
-      #_(contract/wrap-contract)))
+  (let [contracts (merge @contracts/registry
+                         {'f* {:args   (constantly true)
+                               :return (constantly true)}})]
+    (-> f*
+        (e/wrap)
+        (contract/wrap-contract contracts 'f*))))
 
 (defn run [f* & args]
   (with-system [system (test-system)]
     (let [f*                     (wrap f*)
           {:keys [transactable]} system]
       (handlers/with-handlers [handlers transactable]
-        (e/perform handlers (apply f* args))))
-
-
-    #_(let [contracts    (merge @contracts/registry
-                                {'my/f* {:args   (constantly true)
-                                         :return (constantly true)}})
-            continuation (-> f*
-                             (contract/wrap-contract contracts 'my/f*)
-                             #_(context/wrap-context))])))
-      ;;     handlers     (sut/handlers)
-      ;;     perform      (-> e/perform
-      ;;                      (sut/wrap-tx (:transactable system)))
-      ;;     ctx          {}
-      ;;     [ctx result] (perform handlers continuation [ctx args])]
-      ;; result)))
+        (e/perform handlers (apply f* args))))))
